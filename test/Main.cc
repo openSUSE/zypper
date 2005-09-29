f7ba71e9e46d659d0ad626a61b201c2c9e4458e6
Michael Andres <ma@suse.de>
2005-09-29 13:28:06 +0000

    Added libzypp source directory

---
+++ test/Main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+/******************************************************************
+**
+**	FUNCTION NAME : main
+**	FUNCTION TYPE : int
+*/
+int main( int argc, char * argv[] )
+{
+  return 0;
+}
