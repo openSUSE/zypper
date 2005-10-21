46c3dc6734b90f1fe88ea52365420d353f8fd134
Michael Andres <ma@suse.de>
2005-10-21 12:40:25 +0000

    devel stuff

---
+++ test/devel.ma/Main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <zypp/base/Logger.h>
+#include <zypp/Resolvable.h>
+
+using namespace std;
+using namespace zypp;
+
+
+/******************************************************************
+**
+**
+**	FUNCTION NAME : main
+**	FUNCTION TYPE : int
+**
+**	DESCRIPTION :
+*/
+int main( int argc, char * argv[] )
+{
+  INT << "===[START]==========================================" << endl;
+
+
+  INT << "===[END]============================================" << endl;
+  return 0;
+}
