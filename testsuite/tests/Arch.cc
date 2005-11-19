3fa5d1549d22b89a99cbc1f4a0f2ec0c28d24ff9
Klaus Kaempf <kkaempf@suse.de>
2005-11-19 20:12:34 +0000

    enable 'dejagnu' testsuite

---
+++ testsuite/tests/Arch.cc
@@ -0,0 +1,29 @@
+// Arch.cc
+//
+// tests for Arch
+//
+
+#include <iostream>
+#include <list>
+#include <string>
+#include <zypp/base/Logger.h>
+
+#include <zypp/Arch.h>
+
+using namespace std;
+using namespace zypp;
+
+/******************************************************************
+**
+**
+**      FUNCTION NAME : main
+**      FUNCTION TYPE : int
+**
+**      DESCRIPTION :
+*/
+int main( int argc, char * argv[] )
+{
+  Arch        _arch( "i386" );
+
+  return 0;
+}
