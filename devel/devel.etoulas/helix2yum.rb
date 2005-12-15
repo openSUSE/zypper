a60dd4cc4e36fa905820d1d8a9793a36ae56d037
Efthimios Toulas <etoulas@suse.de>
2005-12-15 16:46:06 +0000

    Conversion from Helix to Yum works for now. There still some minor tasks to do: - implement the conversion of the != operator. - read and write checksum, type and flag correct.

--- devel/devel.etoulas/helix2yum.rb
+++ devel/devel.etoulas/helix2yum.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/ruby -w
 
+###
+#
+# ToDos:
+# - find out wether the operator != exists in Yum-dependencies or not
+# - read and write checksum and type correct and set the flag to YES or NO
+#
+###
 require 'optparse'
 require 'rexml/document'
 include REXML
@@ -7,10 +14,10 @@ include REXML
 
 class Dependency
   attr_accessor :name, :op, :version
-  def initialize( name=nil, op=nil, version=nil )
-    @name    = name
-    @op      = op
-    @version = version
+  def initialize( dep_array )
+    @name    = dep_array[0]
+    @op      = dep_array[1]
+    @version = dep_array[2]
   end
   def to_s
     "name=#{@name} op=#{@op} version=#{@version}\n"
@@ -19,7 +26,7 @@ end
 
 class Provides < Dependency
   @@count = 0
-  def initialize( name, op, version )
+  def initialize( p_array )
     super
     @@count += 1
   end
@@ -33,7 +40,7 @@ end
 
 class Conflicts < Dependency
   @@count = 0
-  def initialize( name, op, version )
+  def initialize( c_array )
     super
     @@count += 1
   end
@@ -47,7 +54,7 @@ end
 
 class Requires < Dependency
   @@count = 0
-  def initialize( name, op, version )
+  def initialize( r_array )
     super
     @@count += 1
   end
@@ -73,39 +80,24 @@ class Package
     @ver       = validate_input( pkg, 'history/update/version', '1.0' )     # Version
     @rel       = validate_input( pkg, 'history/update/release', '1'   )     # Release
     @chksum    = validate_input( pkg, 'history/update/md5sum',  '0'   )     # Checksum
-    @chktype   = 'md5sum'   # Checksum type; not needed for now; helix uses md5, yum uses sha-1
-    @pkgid     = 'NO'       # YES if chksum exists
+    @chktype   = 'md5sum'   # Checksum type; not needed for now; helix uses md5, yum uses sha-1 TODO
+    @pkgid     = 'NO'       # YES if chksum exists TODO
     @summary   = validate_input( pkg, 'summary', 'n/a' )
     @descr     = validate_input( pkg, 'description', 'n/a' )   # Package description
     # dependencies
     @provides  = Array.new
     @conflicts = Array.new
     @requires  = Array.new
-    XPath.each( pkg, 'provides/dep'  ) do |p|
-      add_provides(  p.attributes['name'], p.attributes['op'], p.attributes['version'] )
-    end
-    XPath.each( pkg, 'conflicts/dep' ) do |c|
-      add_conflicts( c.attributes['name'], c.attributes['op'], c.attributes['version'] )
-    end
-    XPath.each( pkg, 'requires/dep'  ) do |r|
-      add_requires(  r.attributes['name'], r.attributes['op'], r.attributes['version'] )
-    end
+    XPath.each( pkg, 'provides/dep'  ) { |p| @provides << Provides.new( conv_dependency(p) ) }
+    XPath.each( pkg, 'conflicts/dep' ) { |c| @conflicts << Conflicts.new( conv_dependency(c) ) }
+    XPath.each( pkg, 'requires/dep'  ) { |r| @requires << Requires.new( conv_dependency(r) ) }
   end
 
   def Package.count
     @@count
   end
 
-  def add_provides( name, op, version )
-    @provides << Provides.new( name, op, version )
-  end
-
-  def add_conflicts( name, op, version )
-    @conflicts << Conflicts.new( name, op, version )
-  end
-
-  def add_requires( name, op, version )
-    @requires << Requires.new( name, op, version )
+  def Package.to_yum
   end
 
   def to_s
@@ -120,6 +112,29 @@ class Package
   private
   def validate_input( pkg, xpath, default_val )
     pkg.elements[xpath] == nil || pkg.elements[xpath].text == nil ? default_val : pkg.elements[xpath].text
+  end
+
+  def conv_dependency( dep )
+      dep_array = Array.new(3)
+      dep_array[0] = dep.attributes['name']
+      dep_array[2] = dep.attributes['version']
+      case dep.attributes['op']
+        when '='
+          dep_array[1] = 'EQ'
+        when '!='
+          dep_array[1] = 'TODO'  # Have to check if this operator exists in yum TODO
+        when '&gt;'
+          dep_array[1] = 'GT'
+        when '&ge;'
+          dep_array[1] = 'GE'
+        when '&lt;'
+          dep_array[1] = 'LT'
+        when '&le;'
+          dep_array[1] = 'LE'
+        else
+          dep_array[1,2] = nil
+        end
+      dep_array
   end
 
 end
@@ -188,33 +203,119 @@ while current != nil
   current = current.next_element
 end
 
+puts Package.count.to_s + ' packages processed.'
 
-output << XMLDecl.new
+
+output << XMLDecl.new( '1.0', 'UTF-8' )
 output << Element.new( 'metadata' )
-output.root.add_attribute( 'xmlns', 'http://linux.duke.edu/metadata/common' )
-output.root.add_attribute( 'xmlns:rpm', 'http://linux.duke.edu/metadata/rpm' )
+output.root.add_namespace( 'http://linux.duke.edu/metadata/common' )
+output.root.add_namespace( 'rpm', 'http://linux.duke.edu/metadata/rpm' )
 output.root.add_attribute( 'packages', Package.count )
 
 
-# TODO
-# while !pkg_name.empty?
-#   pkg  = Element.new( 'package' )
-#    pkg.add_attribute( 'type', 'rpm' )
-#   name = Element.new( 'name' ).add_text pkg_name.pop
-#   arch = Element.new( 'arch' ).add_text 'i386'
-#   ver  = Element.new( 'version' )
-#    ver.add_attribute( 'epoch', epoch.pop )
-#    ver.add_attribute( 'ver', version.pop )
-#    ver.add_attribute( 'rel', release.pop )
-#   csum = Element.new( 'checksum' ).add_text md5sum.pop
-#    csum.add_attribute( 'pkgid', 'YES' )
-#    csum.add_attribute( 'type', 'md5sum' )
-#
+# generate the Yum document
+packages.each do |p|
+  pkg  = Element.new( 'package' )
+   pkg.add_attribute( 'type', p.type )
+  name = Element.new( 'name' ).add_text p.name
+  arch = Element.new( 'arch' ).add_text p.arch
+  ver  = Element.new( 'version' )
+   ver.add_attribute( 'epoch', p.epoch )
+   ver.add_attribute( 'ver', p.ver )
+   ver.add_attribute( 'rel', p.rel )
+  csum = Element.new( 'checksum' ).add_text p.chksum
+   csum.add_attribute( 'pkgid', p.pkgid )
+   csum.add_attribute( 'type', p.chktype )
+  sum  = Element.new( 'summary' ).add_text p.summary
+  desc = Element.new( 'description' ).add_text p.descr
+  pker = Element.new( 'packager' ).add_text 'SUSE LINUX Products GmbH. &lt;http://bugzilla.novell.com&gt;'
+  url  = Element.new( 'url' )
+  time = Element.new( 'time' )
+   time.add_attribute( 'file', 'TIMESTAMP' )
+   time.add_attribute( 'build', 'TIMESTAMP' )
+  size = Element.new( 'size' )
+   size.add_attribute( 'package', 'SIZE' )
+   size.add_attribute( 'installed', 'SIZE' )
+   size.add_attribute( 'archive', 'SIZE' )
+  loc  = Element.new( 'location' )
+   loc.add_attribute( 'href', 'PATH/TO/PACKAGE-VERSION.rpm' )
+  form = Element.new( 'format' )
+    licence = Element.new( 'rpm:license' ).add_text 'GPL'
+    vendor  = Element.new( 'rpm:vendor' ) .add_text 'Novell, Inc.'
+    group = Element.new( 'rpm:group' )    .add_text 'SYSTEM/PACKAGEMANAGER'
+    bhost = Element.new( 'rpm:buildhost' ).add_text 'BUILDHOST'
+    srpm = Element.new( 'rpm:sourcerpm' ) .add_text 'PACKAGE-VERSION.src.rpm'
+    hrange = Element.new( 'rpm:header-range' )
+     hrange.add_attribute( 'start', 'VALUE' )
+     hrange.add_attribute( 'end', 'VALUE' )
+
+  # dependencies
+   # provides
+    prov = Element.new( 'rpm:provides' )
+    p.provides.each do |provides|
+       prov.add_element( 'rpm:entry', {
+                                       'name'=>provides.name,
+                                       'flags'=>provides.op,
+                                       'ver'=>provides.version
+                                      }
+                       )
+    end
+   # conflicts
+    conf = Element.new( 'rpm:conflicts' )
+    p.conflicts.each do |conflicts|
+      conf.add_element( 'rpm:entry', {
+                                      'name'=>conflicts.name,
+                                      'flags'=>conflicts.op,
+                                      'ver'=>conflicts.version
+                                     }
+                      )
+    end
+   # requires
+    requ = Element.new( 'rpm:requires' )
+    p.requires.each do |requires|
+      requ.add_element( 'rpm:entry', {
+                                      'name'=>requires.name,
+                                      'flags'=>requires.op,
+                                      'ver'=>requires.version
+                                     }
+                      )
+    end
+
+    file = Element.new( 'file' ).add_text '/PATH/FILE'
+
+    form << licence
+    form << vendor
+    form << group
+    form << bhost
+    form << srpm
+    form << hrange
+    form << prov
+    form << conf
+    form << requ
+    form << file
+
+  pkg << name
+  pkg << arch
+  pkg << ver
+  pkg << csum
+  pkg << sum
+  pkg << desc
+  pkg << pker
+  pkg << url
+  pkg << time
+  pkg << size
+  pkg << loc
+  pkg << form
+
+  output.root << pkg
+end
+
+# 
 #   pkg << name
 #   pkg << arch
 #   pkg << ver
 # #  pkg << csum   # not needed at the moment; helix uses md5, yum uses sha-1
-#
+# 
 #   output.root << pkg
 # end
 
@@ -225,13 +326,13 @@ if not outfile.nil?
   outfile.close
 else
   output.write( $stdout, 0 )
+  puts ''
 end
-puts packages
-puts Package.count.to_s + ' packages processed.'
+#puts packages
 
-rescue => exc
-  STDERR.puts "E: #{exc.message}"
-  exit 1
+# rescue => exc
+#   STDERR.puts "E: #{exc.message}"
+#   exit 1
 ensure
   infile.close
 end
