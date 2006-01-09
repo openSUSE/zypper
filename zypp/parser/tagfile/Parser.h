029fe4d80eb2f38eb8607e1d5cf6fa14601dc5d9
Michael Andres <ma@suse.de>
2006-01-09 09:59:13 +0000

    Basic tagfile parser

---
+++ zypp/parser/tagfile/Parser.h
@@ -0,0 +1,66 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/parser/tagfile/Parser.h
+ *
+*/
+#ifndef ZYPP_PARSER_TAGFILE_PARSER_H
+#define ZYPP_PARSER_TAGFILE_PARSER_H
+
+#include <iosfwd>
+
+#include "zypp/parser/tagfile/Tags.h"
+#include "zypp/parser/tagfile/ParseException.h"
+
+#include "zypp/Pathname.h"
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+  namespace parser
+  { /////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+    namespace tagfile
+    { /////////////////////////////////////////////////////////////////
+
+      ///////////////////////////////////////////////////////////////////
+      //
+      //	CLASS NAME : Parser
+      //
+      /** Tagfile parser. */
+      struct Parser
+      {
+        virtual ~Parser()
+        {}
+
+        /* Overload to consume SingleTag data. */
+        virtual void consume( const STag & stag_r )
+        {}
+        /* Overload to consume MulitTag data. */
+        virtual void consume( const MTag & mtag_r )
+        {}
+
+        /* Parse file and invoke consume on each tag found.
+         * \throw ParseException
+         * \todo more doc on Ecaptions.
+        */
+        void parse( const Pathname & file_r );
+      };
+      ///////////////////////////////////////////////////////////////////
+
+      /////////////////////////////////////////////////////////////////
+    } // namespace tagfile
+    ///////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////
+  } // namespace parser
+  ///////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
+#endif // ZYPP_PARSER_TAGFILE_PARSER_H
