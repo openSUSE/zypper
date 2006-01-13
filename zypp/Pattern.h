7e737322c762c4f56a712c215ed29e5d0f8fc0c7
Klaus Kaempf <kkaempf@suse.de>
2006-01-13 18:35:58 +0000

    prepare for patterns

---
+++ zypp/Pattern.h
@@ -0,0 +1,58 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/Pattern.h
+ *
+*/
+#ifndef ZYPP_PATTERN_H
+#define ZYPP_PATTERN_H
+
+#include "zypp/ResObject.h"
+#include "zypp/detail/PatternImplIf.h"
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  //	CLASS NAME : Pattern
+  //
+  /** Pattern interface.
+  */
+  class Pattern : public ResObject
+  {
+  public:
+    typedef detail::PatternImplIf  Impl;
+    typedef Pattern                Self;
+    typedef ResTraits<Self>          TraitsType;
+    typedef TraitsType::PtrType      Ptr;
+    typedef TraitsType::constPtrType constPtr;
+
+  public:
+    /** */
+    // data here:
+
+  protected:
+    /** Ctor */
+    Pattern( const NVRAD & nvrad_r );
+    /** Dtor */
+    virtual ~Pattern();
+
+  private:
+    /** Access implementation */
+    virtual Impl & pimpl() = 0;
+    /** Access implementation */
+    virtual const Impl & pimpl() const = 0;
+  };
+  ///////////////////////////////////////////////////////////////////
+
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
+#endif // ZYPP_PATTERN_H
