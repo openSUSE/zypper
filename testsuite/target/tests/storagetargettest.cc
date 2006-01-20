499083b41e37a0ddf35f3ee15cc772714e895dae
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-01-20 13:28:42 +0000

    a simple test of writing and reading the same patch 1000 times, meaduring time

---
+++ testsuite/target/tests/storagetargettest.cc
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+ #include <ctime>
+
+#include "boost/filesystem/operations.hpp" // includes boost/filesystem/path.hpp
+#include "boost/filesystem/fstream.hpp"    // ditto
+
+#include <boost/iostreams/device/file_descriptor.hpp>
+
+#include <zypp/base/Logger.h>
+///////////////////////////////////////////////////////////////////
+
+#include <zypp/target/store/PersistentStorage.h>
+#include <zypp/target/store/XMLFilesBackend.h>
+
+#include <zypp/parser/yum/YUMParser.h>
+#include <zypp/base/Logger.h>
+#include <zypp/source/yum/YUMScriptImpl.h>
+#include <zypp/source/yum/YUMMessageImpl.h>
+#include <zypp/source/yum/YUMPackageImpl.h>
+#include <zypp/source/yum/YUMSourceImpl.h>
+
+#include <map>
+#include <set>
+
+#include <zypp/CapFactory.h>
+
+#include <zypp/target/store/serialize.h>
+
+using namespace zypp::detail;
+
+using namespace std;
+using namespace zypp;
+using namespace zypp::parser::yum;
+using namespace zypp::source::yum;
+using namespace zypp::storage;
+
+#define PATCH_FILE "../../../devel/devel.jsrain/repodata/patch.xml"
+
+//using namespace DbXml;
+
+int main()
+{
+	INT << "===[START]==========================================" << endl;
+	YUMSourceImpl src;
+	Patch::Ptr patch1;
+	
+	//YUMPatchParser iter(cin,"");
+	std::ifstream patch_file(PATCH_FILE);
+	YUMPatchParser iter(patch_file,"");
+	for (; !iter.atEnd(); ++iter)
+	{
+		patch1 = src.createPatch(**iter);
+	}
+	if (iter.errorStatus())
+		throw *iter.errorStatus();
+
+  XMLFilesBackend backend;
+  backend.setRandomFileNameEnabled(true);
+  
+  clock_t time_start, curr_time;
+  time_start = clock();
+  int i = 0;
+  for (; i < 1000; i++)
+    backend.storeObject(patch1);
+  curr_time = clock() - time_start;           // time in micro seconds 
+  DBG << "Wrote " << i << " patches in " << curr_time / (float)1000000 << " seconds" << std::endl;
+
+  time_start = clock();
+  std::list<Resolvable::Ptr> objs = backend.storedObjects();
+  curr_time = clock() - time_start;           // time in micro seconds 
+  DBG << "Read " << objs.size() << " patches in " << curr_time / (float)1000000 << " seconds" << std::endl;
+  return 0;	
+}
