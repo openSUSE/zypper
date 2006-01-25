3e42212a4b858a89ccda5c42533b634eaa32961d
Jiri Srain <jsrain@suse.cz>
2006-01-25 12:32:13 +0000

    Added example using SourceManager

--- devel/devel.jsrain/SourceProvide.cc
+++ devel/devel.jsrain/SourceProvide.cc
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <map>
 #include "zypp/base/Logger.h"
-#include "zypp/SourceFactory.h"
+#include "zypp/SourceManager.h"
 #include "zypp/Source.h"
 #include "zypp/source/SourceImpl.h"
 
@@ -20,19 +20,12 @@ using namespace zypp;
 int main( int argc, char * argv[] )
 {
   INT << "===[START]==========================================" << endl;
-  SourceFactory _f;
-  Pathname p = "/";
-  Url url = Url("ftp://cml.suse.cz/netboot/find/SUSE-10.1-CD-OSS-i386-Beta1-CD1");
-//  Url url = Url("http://lide.suse.cz/~~jsrain/devel.jsrain");
-//  Url url = Url("dir:/local/zypp/libzypp/devel/devel.jsrain");
-  Source s = _f.createFrom( url, p );
-  ResStore store = s.resolvables();
-  for (ResStore::const_iterator it = store.begin();
-       it != store.end(); it++)
-  {
-    ERR << **it << endl;
-  }
-  ERR << store << endl;
+  SourceManager_Ptr mgr = SourceManager::sourceManager();
+  unsigned id = mgr->addSource(Url("ftp://cml.suse.cz/netboot/find/SUSE-10.1-CD-OSS-i386-Beta1-CD1"), Pathname("/"));
+  ERR << id << endl;
+  Source & src = mgr->findSource(id);
+  Pathname p = src.provideFile("/control.xml", 0);
+  ERR << p << endl;
   INT << "===[END]============================================" << endl;
   return 0;
 }
