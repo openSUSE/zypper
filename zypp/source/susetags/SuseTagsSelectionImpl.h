b3d69bc12a476e9c25c282defa3682a4d6cdbc17
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-01-27 18:09:33 +0000

    now lets fill the data

--- zypp/source/susetags/SuseTagsSelectionImpl.h
+++ zypp/source/susetags/SuseTagsSelectionImpl.h
@@ -34,6 +34,37 @@ namespace zypp
     public:
       SuseTagsSelectionImpl();
       virtual ~SuseTagsSelectionImpl();
+
+    private:
+      /*
+      virtual std::list<std::string> suggests() const;
+      virtual std::list<std::string> recommends() const;
+      virtual std::list<std::string> insnotify( const LangCode & lang = LangCode("") ) const;
+      virtual std::list<std::string> delnotify( const LangCode & lang = LangCode("") ) const;
+      virtual ByteCount size() const;
+      virtual bool providesSources() const;
+      virtual std::string instSrcLabel() const;
+      virtual Vendor instSrcVendor() const;
+      virtual unsigned instSrcRank() const;
+      virtual std::list<PMSelectionPtr> suggests_ptrs() const;
+      virtual std::list<PMSelectionPtr> recommends_ptrs() const;
+      virtual std::list<std::string> inspacks( const LangCode & lang = LangCode("") ) const;
+      virtual std::list<std::string> delpacks( const LangCode & lang = LangCode("") ) const;
+      virtual PM::LocaleSet supportedLocales() const;
+      virtual std::set<PMSelectablePtr> pureInspacks_ptrs( const LangCode & lang ) const;
+      virtual std::set<PMSelectablePtr> inspacks_ptrs( const LangCode & lang ) const;
+      virtual std::set<PMSelectablePtr> delpacks_ptrs( const LangCode & lang ) const;
+      virtual bool isBase() const;
+      virtual PMError provideSelToInstall( Pathname & path_r ) const;
+      */
+      std::set<std::string> _suggests;
+      std::set<std::string> _recommends;
+      std::set<std::string> _supported_locales;
+      std::map< std::string, std::set<std::string> > _insnotify;
+      std::map< std::string, std::set<std::string> > _delnotify;
+      std::map< std::string, std::set<std::string> > _inspacks;
+      std::map< std::string, std::set<std::string> > _delpacks;
+      
     };
     ///////////////////////////////////////////////////////////////////
 
