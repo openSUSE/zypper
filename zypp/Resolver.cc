89ec68a8f1db2772123c227074f776ced7f7bc73
Klaus Kaempf <kkaempf@suse.de>
2006-01-29 15:35:45 +0000

    pull Resolver to toplevel API

---
+++ zypp/Resolver.cc
@@ -0,0 +1,69 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/Resolver.cc
+ *
+*/
+#include <iostream>
+
+#include "zypp/Resolver.h"
+#include "zypp/solver/detail/Resolver.h"
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+
+  IMPL_PTR_TYPE(Resolver);
+#if 0
+  Resolver_Ptr Resolver::_resolver = NULL;
+  Resolver_Ptr Resolver::resolver()
+  {
+    if (_resolver == NULL) {
+	_resolver = new Resolver();
+    }
+    return _resolver;
+  }
+#endif
+  ///////////////////////////////////////////////////////////////////
+  //
+  //	METHOD NAME : Resolver::Resolver
+  //	METHOD TYPE : Ctor
+  //
+  Resolver::Resolver( const ResPool & pool )
+  {
+    _pimpl = new solver::detail::Resolver(pool);
+  }
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  //	METHOD NAME : Resolver::~Resolver
+  //	METHOD TYPE : Dtor
+  //
+  Resolver::~Resolver()
+  {}
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  //	Resolver interface forwarded to implementation
+  //
+  ///////////////////////////////////////////////////////////////////
+
+  void Resolver::verifySystem ()
+  { return _pimpl->verifySystem(); }
+  void Resolver::establishState ()
+  { return _pimpl->establishState(); }
+  bool Resolver::resolveDependencies ()
+  { return _pimpl->resolveDependencies(); }
+
+  // ResolverContext_constPtr bestContext (void) const;
+
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
---
+++ zypp/Resolver.h
@@ -0,0 +1,103 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/Resolver.h
+ *
+*/
+#ifndef ZYPP_RESOLVER_H
+#define ZYPP_RESOLVER_H
+
+#include <iosfwd>
+#include <functional>
+
+#include "zypp/base/ReferenceCounted.h"
+#include "zypp/base/PtrTypes.h"
+
+
+#include "zypp/ResPool.h"
+#include "zypp/solver/detail/Resolver.h"
+#include "zypp/solver/detail/ResolverContext.h"
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+
+  DEFINE_PTR_TYPE(Resolver);
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  //	CLASS NAME : Resolver
+  //
+  /** Resolver interface.
+  */
+  class Resolver : public base::ReferenceCounted, private base::NonCopyable
+  {
+  public:
+
+  protected:
+    /** Ctor */
+    Resolver( const ResPool & pool );
+    /** Dtor */
+    virtual ~Resolver();
+
+    /**
+     * Resolve package dependencies:
+     *
+     * Verify consistency of system
+     *
+     **/
+    void verifySystem ();
+
+    /**
+     * Establish state of 'higher level' Resolvables
+     *
+     * Must be called when dealing with non-package resolvables,
+     * like Patches, Patterns, and Products
+     *
+     **/
+    void establishState ();
+
+    /**
+     * Resolve package dependencies:
+     *
+     * Try to execute all pending transactions (there may be more than
+     * one!).
+     *
+     * Returns "true" on success (i.e., if there were no problems that
+     * need user interaction) and "false" if there were problems.  In
+     * the latter case, use problems() and later applySolutions()
+     * below.
+     **/
+    bool resolveDependencies ();
+
+    /**
+     * Return the dependency problems found by the last call to
+     * resolveDependencies(). If there were no problems, the returned
+     * list will be empty.
+     **/
+//    list<ResolverProblem_Ptr> problems();
+
+    /**
+     * Apply problem solutions. No more than one solution per problem
+     * can be applied.
+     **/
+//    void applySolutions( list<ProblemSolution_Ptr> solutions );
+
+    // ResolverContext_constPtr bestContext (void) const;
+
+  protected:
+
+  private:
+    solver::detail::Resolver_Ptr _pimpl;
+  };
+  ///////////////////////////////////////////////////////////////////
+
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
+#endif // ZYPP_RESOLVER_H
