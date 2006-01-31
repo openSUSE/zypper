b21e22b665a6b361d7b27575b354ea51e88c78c1
Marius Tomaschewski <mt@suse.de>
2006-01-31 17:50:21 +0000

    - Added media testsuite directory

---
+++ testsuite/media/tests/media1.cc
@@ -0,0 +1,67 @@
+#include <zypp/media/MediaManager.h>
+#include <zypp/base/String.h>
+#include <zypp/base/Logger.h>
+#include <zypp/Pathname.h>
+
+#include <string>
+#include <list>
+#include <iostream>
+
+using namespace zypp;
+using namespace zypp::media;
+
+class MyMediaVerifier: public MediaVerifierBase
+{
+public:
+  MyMediaVerifier()
+    : MediaVerifierBase()
+  {}
+
+  virtual
+  ~MyMediaVerifier()
+  {}
+
+  virtual bool
+  isDesiredMedia(MediaAccessRef &ref, MediaNr mediaNr)
+  {
+    DBG << "isDesiredMedia(): for media nr " << mediaNr << std::endl;
+
+    if( !ref)
+      DBG << "isDesiredMedia(): invalid media handle" << std::endl;
+
+    std::list<std::string> lst;
+    Pathname               dir("/media." + str::numstring(mediaNr));
+
+    DBG << "isDesiredMedia(): checking " << dir.asString() << std::endl;
+
+    try
+    {
+      if( ref)
+        ref->dirInfo(lst, dir, false);
+    }
+    catch( ... )
+    {}
+    DBG << "isDesiredMedia(): media "
+        << (lst.empty() ? "does not contain" : "contains")
+        << " the " << dir.asString() << " directory."
+        << std::endl;
+
+    return !lst.empty();
+  }
+};
+
+int main(void)
+{
+  MediaVerifierRef v(new MyMediaVerifier());
+  MediaManager mm;
+  media::MediaId      id;
+
+  id = mm.open(zypp::Url("cd:"));
+  mm.addVerifier( id, v);
+
+  mm.provideFile(id, 1, Pathname("/directory.yast"));
+
+  return 0;
+}
+
+// vim: set ts=2 sts=2 sw=2 ai et:
