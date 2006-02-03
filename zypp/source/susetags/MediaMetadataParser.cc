fc162165862104c5213b5f7a4eb58688e9c40cc3
Klaus Kaempf <kkaempf@suse.de>
2006-02-03 08:18:20 +0000

    throw more

--- zypp/source/susetags/MediaMetadataParser.cc
+++ zypp/source/susetags/MediaMetadataParser.cc
@@ -40,31 +40,31 @@ namespace zypp
 
       static void dumpRegexpResults( const boost::smatch &what )
       {
-        for ( unsigned int k=0; k < what.size(); k++)
-        {
-          DBG << "[match "<< k << "] [" << what[k] << "]" << std::endl; 
-        }
+	for ( unsigned int k=0; k < what.size(); k++)
+	{
+	  DBG << "[match "<< k << "] [" << what[k] << "]" << std::endl; 
+	}
       }
       
       /*
-        File:  media
-        Location  /media.N/ directory on media
-        Content  two or more lines of ASCII as follows
-        <vendor>
-        <YYYYMMDDHHMMSS>
-        [<media count>]
-        [<media flags>]
-        [<media names>]
+	File:  media
+	Location  /media.N/ directory on media
+	Content  two or more lines of ASCII as follows
+	<vendor>
+	<YYYYMMDDHHMMSS>
+	[<media count>]
+	[<media flags>]
+	[<media names>]
 
-        Currently defined flags:
-         
-        doublesided 
-        media is double sided, YaST will ask for 'front side' for odd-numbered media and 'back side' for even-numbered media.
-        The default is single-sided media.
+	Currently defined flags:
+	 
+	doublesided 
+	media is double sided, YaST will ask for 'front side' for odd-numbered media and 'back side' for even-numbered media.
+	The default is single-sided media.
 
-        <media names> may define alternate strings to use when asking to insert a certain media.
-         They are defined as <key><whitespace><value> pairs, separated by \n.
-         
+	<media names> may define alternate strings to use when asking to insert a certain media.
+	 They are defined as <key><whitespace><value> pairs, separated by \n.
+	 
       */
       
       ///////////////////////////////////////////////////////////////////
@@ -74,69 +74,70 @@ namespace zypp
       //
       void MediaMetadataParser::parse( const Pathname & file_r, MediaEntry &entry_r )
       {
-        std::ifstream file(file_r.asString().c_str());
+	std::ifstream file(file_r.asString().c_str());
 	if (!file) {
 	    ZYPP_THROW(Exception("Can't read media file "+file_r.asString()));
 	}
 
-        std::string buffer;
-        // read vendor
-        getline(file, entry_r.vendor);
-        // read timestamp
-        getline(file, entry_r.timestamp);
-        // skip flags for now
-        std::string media_count_str;
-        
-        getline(file, buffer);
-        regex is_digit_rx("^[\\d]+$");
-        boost::smatch what_digit;
-        
-        // for the first line here we dont have to consume one if
-        // there was no media
-        bool consume = false;  
+	std::string buffer;
+	// read vendor
+	getline(file, entry_r.vendor);
+	// read timestamp
+	getline(file, entry_r.timestamp);
+	// skip flags for now
+	std::string media_count_str;
+	
+	getline(file, buffer);
+	regex is_digit_rx("^[\\d]+$");
+	boost::smatch what_digit;
+	
+	// for the first line here we dont have to consume one if
+	// there was no media
+	bool consume = false;  
 
-        if(boost::regex_match(buffer, what_digit, is_digit_rx))
-        {
-          // it was the media count
-          str::strtonum(buffer, entry_r.count);
-          // consume another line
-          consume = true;
-        }
-        else
-        {
-          // media count defaults to 1
-          entry_r.count = 1;
-        }
-        
-        while(file && !file.eof())
-        {
-          // probably is the first line after we dont find the media number
-          if(consume)
-            getline(file, buffer);
-          
-          // only skip once
-          consume = true;
-          boost::regex e("^MEDIA([\\d]+)(\\.([_A-Za-z]+)){0,1} (.+)$");
-          boost::smatch what;
-          if(boost::regex_match(buffer, what, e, boost::match_extra))
-          {
-            if ( what.size() < 5 )
-              std::cout << "ups!!!!" << std::endl;
-           
-            dumpRegexpResults(what);
-            
-            unsigned int number = 1;
-            str::strtonum( what[1], number);
-            std::string lang = what[3];
-            std::string desc = what[4];
-            entry_r.alternate_names[number][lang] = desc;
-          }
-          else
-          {
-            DBG << "** No Match found:  " << buffer << std::endl;
-          }
-          
-        }
+	if(boost::regex_match(buffer, what_digit, is_digit_rx))
+	{
+	  // it was the media count
+	  str::strtonum(buffer, entry_r.count);
+	  // consume another line
+	  consume = true;
+	}
+	else
+	{
+	  // media count defaults to 1
+	  entry_r.count = 1;
+	}
+	
+	while(file && !file.eof())
+	{
+	  // probably is the first line after we dont find the media number
+	  if (consume)
+	    getline(file, buffer);
+	  
+	  // only skip once
+	  consume = true;
+	  boost::regex e("^MEDIA([\\d]+)(\\.([_A-Za-z]+)){0,1} (.+)$");
+	  boost::smatch what;
+	  if(boost::regex_match(buffer, what, e, boost::match_extra))
+	  {
+	    if ( what.size() < 5 ) {
+	      ZYPP_THROW (Exception("Can't match MEDIA in '" + buffer + "'"));
+	    }
+	   
+	    dumpRegexpResults(what);
+	    
+	    unsigned int number = 1;
+	    str::strtonum( what[1], number);
+	    std::string lang = what[3];
+	    std::string desc = what[4];
+	    entry_r.alternate_names[number][lang] = desc;
+	  }
+	  else
+	  {
+	      ZYPP_THROW (Exception("Can't find MEDIA in '" + buffer + "'"));
+	  }
+	  
+	}
       }
 
        /////////////////////////////////////////////////////////////////
