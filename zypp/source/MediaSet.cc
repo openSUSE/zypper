fc52268785f41f200a0bdcf662125bdea6533484
Jiri Srain <jsrain@suse.cz>
2006-02-10 00:02:12 +0000

    added MediaSet class

---
+++ zypp/source/MediaSet.cc
@@ -0,0 +1,103 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/source/MediaSet.cc
+ *
+*/
+#include <iostream>
+#include "zypp/base/Logger.h"
+
+#include "zypp/SourceFactory.h"
+#include "zypp/source/MediaSet.h"
+#include "zypp/ZYppCallbacks.h"
+
+#include <fstream>
+
+using std::endl;
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+  namespace source
+  { /////////////////////////////////////////////////////////////////
+
+    IMPL_PTR_TYPE(MediaSet);
+
+    media::MediaManager media_mgr;
+    MediaSet::MediaSet(const Source_Ref & source_r)
+    {
+      _source = source_r;
+    }
+
+    void MediaSet::redirect (media::MediaNr medianr, media::MediaAccessId media_id)
+    {
+      medias[medianr] = media_id;
+    }
+
+    void MediaSet::reset()
+    {
+      medias = MediaMap();
+    }
+
+    media::MediaAccessId MediaSet::getMediaAccessId (media::MediaNr medianr)
+    {
+      if (medias.find(medianr) != medias.end())
+      {
+	media::MediaAccessId id = medias[medianr];
+	if (! media_mgr.isAttached(id))
+	  media_mgr.attach(id);
+	return id;
+      }
+      Url url = _source.url();
+      url = rewriteUrl (url, medianr);
+      media::MediaAccessId id = media_mgr.open(url, _source.path());
+      try {
+	MIL << "Adding media verifier" << endl;
+	media_mgr.delVerifier(id);
+	media_mgr.addVerifier(id, _source.verifier(medianr));
+      }
+      catch (const Exception & excpt_r)
+      {
+#warning FIXME: If media data is not set, verifier is not set. Should the media be refused instead?
+	ZYPP_CAUGHT(excpt_r);
+	WAR << "Verifier not found" << endl;
+      }
+      medias[medianr] = id;
+      media_mgr.attach(id);
+      return id;
+    }
+
+    Url MediaSet::rewriteUrl (const Url & url_r, const media::MediaNr medianr)
+    {
+      std::string scheme = url_r.getScheme();
+      if (scheme == "cd" || scheme == "dvd")
+	return url_r;
+      std::string pathname = url_r.getPathName();
+      boost::regex e("^(.*)[0-9]+$");
+      boost::smatch what;
+      if(boost::regex_match(pathname, what, e, boost::match_extra))
+      {
+	std::string base = what[1];
+	pathname = base + str::numstring(medianr);
+	Url url = url_r;
+	url.setPathName (pathname);
+      }
+      return url_r;
+    }
+
+    std::ostream & MediaSet::dumpOn( std::ostream & str ) const
+    { return str << "MediaSet"; }
+
+
+    /////////////////////////////////////////////////////////////////
+  } // namespace source
+  ///////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
---
+++ zypp/source/MediaSet.h
@@ -0,0 +1,80 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/source/MediaSet.h
+ *
+*/
+#ifndef ZYPP_SOURCE_MEDIASET_H
+#define ZYPP_SOURCE_MEDIASET_H
+
+#include <iosfwd>
+#include <string>
+
+#include "zypp/base/ReferenceCounted.h"
+#include "zypp/base/NonCopyable.h"
+#include "zypp/base/PtrTypes.h"
+#include "zypp/Source.h"
+
+#include "zypp/media/MediaManager.h"
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+  namespace source
+  { /////////////////////////////////////////////////////////////////
+
+    DEFINE_PTR_TYPE(MediaSet);
+
+    ///////////////////////////////////////////////////////////////////
+    //
+    //	CLASS NAME : MediaSet
+    //
+    class MediaSet : public base::ReferenceCounted, private base::NonCopyable
+    {
+      friend std::ostream & operator<<( std::ostream & str, const MediaSet & obj );
+
+    public:
+      MediaSet(const Source_Ref & source_r);
+
+      /** Get the media access ID to specified media */
+      media::MediaAccessId getMediaAccessId (media::MediaNr medianr);
+      /** Redirect specified media to a new MediaId */
+      void redirect (media::MediaNr medianr, media::MediaAccessId media_id);
+      /** Reset the handles to the medias */
+      void reset();
+
+    protected:
+
+      /** Overload to realize stream output. */
+      virtual std::ostream & dumpOn( std::ostream & str ) const;
+
+      typedef std::map<media::MediaNr, media::MediaAccessId> MediaMap;
+      /** Mapping between each CD and Media Access ID */
+      MediaMap medias;
+      /** Refference to the source */
+      Source_Ref _source;
+
+      /** Rewrite the URL according to media number */
+      Url rewriteUrl (const Url & url_r, const media::MediaNr medianr);
+
+    };
+    ///////////////////////////////////////////////////////////////////
+
+    /** \relates MediaSet Stream output */
+    inline std::ostream & operator<<( std::ostream & str, const MediaSet & obj )
+    { return obj.dumpOn( str ); }
+
+
+    /////////////////////////////////////////////////////////////////
+  } // namespace source
+  ///////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
+#endif // ZYPP_SOURCE_MEDIASET_H
