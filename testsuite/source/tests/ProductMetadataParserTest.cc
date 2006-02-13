9d62020d117af1d7fa2d1beb141bb600d6a9f199
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-02-13 14:31:37 +0000

    compile

--- testsuite/source/tests/ProductMetadataParserTest.cc
+++ testsuite/source/tests/ProductMetadataParserTest.cc
@@ -4,6 +4,7 @@
 #include "zypp/base/Logger.h"
 #include "zypp/base/Exception.h"
 #include "zypp/Pathname.h"
+#include "zypp/Product.h"
 
 using namespace zypp;
 using namespace zypp::source::susetags;
@@ -11,13 +12,12 @@ using namespace zypp::source::susetags;
 int main()
 {
   ProductMetadataParser parser;
-  ProductMetadataParser::ProductEntry entry;
-
+  Product::Ptr product;
   try {
-  parser.parse(Pathname("products/content.1.txt"), entry);
-  DBG << "arch: " << entry.arch["x86_64"].size() << std::endl;
-  parser.parse(Pathname("products/NOTTHERE.txt"), entry);
-  DBG << "arch: " << entry.arch["x86_64"].size() << std::endl;
+  product = parseContentFile(Pathname("products/content.1.txt"));
+  //DBG << "arch: " << entry.arch["x86_64"].size() << std::endl;
+  product = parseContentFile(Pathname("products/NOTTHERE.txt"));
+  //DBG << "arch: " << entry.arch["x86_64"].size() << std::endl;
   }
   catch (Exception & excpt_r) {
 	ZYPP_CAUGHT (excpt_r);
