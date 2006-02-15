e5f63ea0992205d0364da8dc444a307ab65b249b
Klaus Kaempf <kkaempf@suse.de>
2006-02-15 15:09:02 +0000

    shorten debug output

--- zypp/solver/detail/ResolverInfo.cc
+++ zypp/solver/detail/ResolverInfo.cc
@@ -45,52 +45,69 @@ IMPL_PTR_TYPE(ResolverInfo);
 
 //---------------------------------------------------------------------------
 
+#define RIT(x) RESOLVER_INFO_TYPE_ ## x, #x
 static struct {
     ResolverInfoType type;
-    const char        *str;
+    const char	*typestr;
+    const char  *str;
 } type_str_table[] = {
-    { RESOLVER_INFO_TYPE_NEEDED_BY,			"needed_by" },
-    { RESOLVER_INFO_TYPE_CONFLICTS_WITH,		"conflicts_with" },
-    { RESOLVER_INFO_TYPE_OBSOLETES,			"obsoletes" },
-    { RESOLVER_INFO_TYPE_DEPENDS_ON,			"depended_on" },
-    { RESOLVER_INFO_TYPE_CHILD_OF,			"child_of" },
-    { RESOLVER_INFO_TYPE_MISSING_REQ,			"missing_req" },
+    { RIT(NEEDED_BY),			"needed_by" },
+    { RIT(CONFLICTS_WITH),		"conflicts_with" },
+    { RIT(OBSOLETES),			"obsoletes" },
+    { RIT(DEPENDS_ON),			"depended_on" },
+    { RIT(CHILD_OF),			"child_of" },
+    { RIT(MISSING_REQ),			"missing_req" },
 
-    { RESOLVER_INFO_TYPE_INVALID_SOLUTION,		"Marking this resolution attempt as invalid" },
-    { RESOLVER_INFO_TYPE_UNINSTALLABLE,			"Marking p as uninstallable" },
-    { RESOLVER_INFO_TYPE_REJECT_INSTALL,		"p is scheduled to be installed, but this is not possible because of dependency problems." },
-    { RESOLVER_INFO_TYPE_INSTALL_TO_BE_UNINSTALLED,	"Can't install p since it is already marked as needing to be uninstalled" },
-    { RESOLVER_INFO_TYPE_INSTALL_UNNEEDED,		"Can't install p since it is does not apply to this system." },
-    { RESOLVER_INFO_TYPE_INSTALL_PARALLEL,		"Can't install p, since a resolvable of the same name is already marked as needing to be installed." },
-    { RESOLVER_INFO_TYPE_INCOMPLETES,			"This would invalidate p" },
+    { RIT(INVALID_SOLUTION),		"Marking this resolution attempt as invalid" },
+    { RIT(UNINSTALLABLE),		"Marking p as uninstallable" },
+    { RIT(REJECT_INSTALL),		"p is scheduled to be installed, but this is not possible because of dependency problems." },
+    { RIT(INSTALL_TO_BE_UNINSTALLED),	"Can't install p since it is already marked as needing to be uninstalled" },
+    { RIT(INSTALL_UNNEEDED),		"Can't install p since it is does not apply to this system." },
+    { RIT(INSTALL_PARALLEL),		"Can't install p, since a resolvable of the same name is already marked as needing to be installed." },
+    { RIT(INCOMPLETES),			"This would invalidate p" },
 	// from QueueItemEstablish
-    { RESOLVER_INFO_TYPE_ESTABLISHING,			"Establishing p" },
+    { RIT(ESTABLISHING),		"Establishing p" },
 	// from QueueItemInstall
-    { RESOLVER_INFO_TYPE_INSTALLING,			"Installing p" },
-    { RESOLVER_INFO_TYPE_UPDATING,			"Updating p" },
-    { RESOLVER_INFO_TYPE_SKIPPING,			"Skipping p, already installed" },
+    { RIT(INSTALLING),			"Installing p" },
+    { RIT(UPDATING),			"Updating p" },
+    { RIT(SKIPPING),			"Skipping p, already installed" },
 	// from QueueItemRequire
-    { RESOLVER_INFO_TYPE_NO_OTHER_PROVIDER,		"There are no alternative installed providers of c [for p]" },
-    { RESOLVER_INFO_TYPE_NO_PROVIDER,			"There are no installable providers of c [for p]" },
-    { RESOLVER_INFO_TYPE_NO_UPGRADE,			"Upgrade to q to avoid removing p is not possible." },
-    { RESOLVER_INFO_TYPE_UNINSTALL_PROVIDER,		"p provides c but is scheduled to be uninstalled" },
-    { RESOLVER_INFO_TYPE_PARALLEL_PROVIDER,		"p provides c but another version is already installed" },
-    { RESOLVER_INFO_TYPE_NOT_INSTALLABLE_PROVIDER,	"p provides c but is uninstallable" },
-    { RESOLVER_INFO_TYPE_LOCKED_PROVIDER,		"p provides c but is locked" },
-    { RESOLVER_INFO_TYPE_CANT_SATISFY,			"Can't satisfy requirement c" },
+    { RIT(NO_OTHER_PROVIDER),		"There are no alternative installed providers of c [for p]" },
+    { RIT(NO_PROVIDER),			"There are no installable providers of c [for p]" },
+    { RIT(NO_UPGRADE),			"Upgrade to q to avoid removing p is not possible." },
+    { RIT(UNINSTALL_PROVIDER),		"p provides c but is scheduled to be uninstalled" },
+    { RIT(PARALLEL_PROVIDER),		"p provides c but another version is already installed" },
+    { RIT(NOT_INSTALLABLE_PROVIDER),	"p provides c but is uninstallable" },
+    { RIT(LOCKED_PROVIDER),		"p provides c but is locked" },
+    { RIT(CANT_SATISFY),		"Can't satisfy requirement c" },
 	// from QueueItemUninstall
-    { RESOLVER_INFO_TYPE_UNINSTALL_TO_BE_INSTALLED,	"p is to-be-installed, so it won't be unlinked." },
-    { RESOLVER_INFO_TYPE_UNINSTALL_INSTALLED,		"p is required by installed, so it won't be unlinked." },
-    { RESOLVER_INFO_TYPE_UNINSTALL_LOCKED,		"cant uninstall, its locked" },
+    { RIT(UNINSTALL_TO_BE_INSTALLED),	"p is to-be-installed, so it won't be unlinked." },
+    { RIT(UNINSTALL_INSTALLED),		"p is required by installed, so it won't be unlinked." },
+    { RIT(UNINSTALL_LOCKED),		"cant uninstall, its locked" },
 	// from QueueItemConflict
-    { RESOLVER_INFO_TYPE_CONFLICT_CANT_INSTALL,		"to-be-installed p conflicts with q due to c" },
-    { RESOLVER_INFO_TYPE_CONFLICT_UNINSTALLABLE,	"uninstalled p is marked uninstallable it conflicts [with q] due to c" },
-    { RESOLVER_INFO_TYPE_INVALID,			"invalid" },
-    { RESOLVER_INFO_TYPE_INVALID, NULL }
+    { RIT(CONFLICT_CANT_INSTALL),	"to-be-installed p conflicts with q due to c" },
+    { RIT(CONFLICT_UNINSTALLABLE),	"uninstalled p is marked uninstallable it conflicts [with q] due to c" },
+    { RIT(INVALID),			"invalid" },
+    { RIT(INVALID), NULL }
 };
+#undef RIT
 
 static const char *
 info_type_to_string (ResolverInfoType type)
+{
+    int i;
+
+    for (i = 0; type_str_table[i].str != NULL; ++i) {
+	if (type == type_str_table[i].type)
+	    return type_str_table[i].typestr;
+    }
+
+    return "?ResolverInfoType?";
+}
+
+
+static const char *
+info_type_to_message (ResolverInfoType type)
 {
     int i;
 
@@ -154,7 +171,7 @@ ResolverInfo::toString (const Capability & capability)
 string
 ResolverInfo::message( ) const
 {
-    return string(info_type_to_string(_type)) + " " + toString(_affected);
+    return string(info_type_to_message(_type)) + " " + toString(_affected);
 }
 //---------------------------------------------------------------------------
 
