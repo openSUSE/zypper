14d4fdfd51dac696aec7e790b038b6d68c5d40c9
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-02-20 13:55:56 +0000

    add testcase for Huha bug

--- testsuite/source/tests/PatternTagParserTest.cc
+++ testsuite/source/tests/PatternTagParserTest.cc
@@ -11,10 +11,18 @@ int main()
 {
   Pattern::Ptr pattern;
   try {
-  pattern = zypp::source::susetags::parsePattern (Pathname("patfiles/default.pat"));
-  cout << *pattern << endl;
-  pattern = zypp::source::susetags::parsePattern (Pathname("patfiles/NOTTHERE.pat"));
-  cout << *pattern << endl;
+    pattern = zypp::source::susetags::parsePattern (Pathname("patfiles/default.pat"));
+    cout << *pattern << endl;
+    pattern = zypp::source::susetags::parsePattern (Pathname("patfiles/NOTTHERE.pat"));
+    cout << *pattern << endl;
+    
+    pattern = zypp::source::susetags::parsePattern (Pathname("patfiles/base-10-33.i586.pat"));
+    if (pattern->userVisible())
+    {
+      ERR << "Error parsing userVisible" << std::endl;
+      return 1;
+    }
+    cout << *pattern << endl;
   }
   catch (Exception & excpt_r) {
     ZYPP_CAUGHT (excpt_r);
