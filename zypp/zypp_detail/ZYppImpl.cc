d3f2a7bdc555dffad2c2adaf6f85c97ad06ad4ba
Klaus Kaempf <kkaempf@suse.de>
2006-02-23 20:44:31 +0000

    return on multiple calls to initTarget()

--- zypp/zypp_detail/ZYppImpl.cc
+++ zypp/zypp_detail/ZYppImpl.cc
@@ -104,8 +104,11 @@ namespace zypp
 
     void ZYppImpl::initTarget(const Pathname & root, bool commit_only)
     {
-      if (_target)
-	_target = Target_Ptr();
+      if (_target) {
+	WAR << "Repeated call to initTarget()" << endl;
+        return;
+      }
+
       _target = new Target(root);
       if (!commit_only)
       {
