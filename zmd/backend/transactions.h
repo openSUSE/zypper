20435eb6535b8205ed02052b3b380922a10025e6
Klaus Kaempf <kkaempf@suse.de>
2006-03-07 19:58:30 +0000

    move zmd-backend out of library build into a separate directory   (with separate "make dist" and a separate package)

--- zmd/backend/transactions.h
+++
@@ -1,9 +0,0 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 4 -*- */
-
-#include "dbsource/DbSources.h"
-#include "zypp/ResPool.h"
-#include "zypp/solver/detail/ResolverContext.h"
-#include <sqlite3.h>
-
-extern bool read_transactions (const zypp::ResPool & pool, sqlite3 *db, const DbSources & sources);
-extern bool write_transactions (const zypp::ResPool & pool, sqlite3 *db, zypp::solver::detail::ResolverContext_Ptr context);
