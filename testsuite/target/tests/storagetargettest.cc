6c838a2f8fa26ba8dd00169d2ea4d8f0f7c1e2d0
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-03-07 10:28:50 +0000

    more tests

--- testsuite/target/tests/storagetargettest.cc
+++ testsuite/target/tests/storagetargettest.cc
@@ -46,7 +46,19 @@ using namespace boost::filesystem;
 
 #define PATCH_FILE "../../../devel/devel.jsrain/repodata/patch.xml"
 
-//using namespace DbXml;
+typedef std::list<ResObject::Ptr> ResObjectPtrList;
+
+static std::string dump( const CapSet &caps )
+{
+  stringstream out;
+  CapSet::iterator it = caps.begin();
+  for ( ; it != caps.end(); ++it)
+  {
+    out << (*it).asString() << std::endl;
+  }
+  return out.str();
+}
+
 
 struct Benchmark
 {
@@ -191,7 +203,32 @@ struct StorageTargetTest
     clean();
     unpackDatabase("db.tar.gz");
     initStorageBackend();
-    std::list<ResObject::Ptr> objs = readStoreResolvables();
+    ResObjectPtrList objs = readStoreResolvables();
+    for ( ResObjectPtrList::const_iterator it = objs.begin(); it != objs.end(); it++)
+    {
+      ResObject::Ptr res = *it;
+      if ( isKind<Selection>( res ) && res->name() == "Multimedia" )
+      {
+        // requires basis-sound and x11
+        if ( res->deps()[Dep::REQUIRES].size() != 2 )     
+        {
+          ERR << dump( res->deps()[Dep::REQUIRES] ) ;   
+          ZYPP_THROW(Exception("Parsed of requires for Multimedia Selection failed.")); 
+        }
+        // provides multimedia and multimedia == version
+        if ( res->deps()[Dep::PROVIDES].size() != 2 )
+        {        
+          ERR << dump( res->deps()[Dep::PROVIDES] ) ;
+          ZYPP_THROW(Exception("Parsed of provides for Multimedia Selection failed.")); 
+        }
+        // recommends like 100 packages
+        if ( res->deps()[Dep::RECOMMENDS].size() < 60 )        
+        {
+          ERR << dump( res->deps()[Dep::RECOMMENDS] ) ;
+          ZYPP_THROW(Exception("Parsed of recommends for Multimedia Selection failed."));
+        }
+      }
+    }
     clean();
     return 0;
   }
@@ -263,6 +300,35 @@ struct StorageTargetTest
     initStorageBackend();
     writeResolvablesInStore();
     readStoreResolvables();
+    s.storeMetadata("./source-cache");
+    */
+    /*
+    // now write the files
+    DBG << "Writing objects..." << std::endl;
+    for (ResStore::const_iterator it = store.begin(); it != store.end(); it++)
+    {
+    DBG << **it << endl;
+    backend.storeObject(*it);
+    //backend.setObjectFlag(*it, "blah1");
+    //backend.setObjectFlag(*it, "locked");
+    //backend.setObjectFlag(*it, "license-acepted");
+   
+  }
+    
+    for ( ResStore::const_iterator it = store.begin(); it != store.end(); it++)
+    {
+    std::set<std::string> flags = backend.objectFlags(*it);
+    if ( flags.size() != 3 )
+    {
+    for ( std::set<std::string>::const_iterator itf = flags.begin(); itf != flags.end(); ++it)
+    {
+        //ERR << "Tag " << *itf << std::endl;
+  }
+      
+      //ERR << "Saved 3 tags, read " << flags.size() << " for " << **it << std::endl;
+      //ZYPP_THROW(Exception("Saved 3 tags, read other")); 
+  }
+  }
     */
     return 0;
   }
@@ -273,113 +339,8 @@ struct StorageTargetTest
     
   }
   
-  int publickeys_test()
-  {
-    MIL << "===[START: publickeys_test()]==========================================" << endl;
-    
-  }
-  
-  
 };
 
-
-static int readAndStore()
-{
-  
-  /* Read YUM resolvables from a source */
-  INT << "===[START]==========================================" << endl;
-  SourceFactory _f;
-  Pathname p = "/";
-  Pathname root("."); 
-  //Url url("cd:///");
-  //static Url url = Url("ftp://cml.suse.cz/netboot/find/SUSE-10.1-CD-OSS-i386-Beta1-CD1");
-//static Url url = Url("dir:/space/sources/zypp-trunk/trunk/libzypp/devel/devel.jsrain");
-//static Url url("ftp://machcd2.suse.de/SLES/SUSE-Linux-10.1-beta5-i386/CD1");
-  Url url = Url("dir:/mounts/dist/10.0-i386");
-//Url url = Url("dir:/space/zypp-test/sles-beta5");
-  Source_Ref s = _f.createFrom( url, p, "testsource", "./source-cache" );
-  //Source_Ref s = _f.createFrom( url, p, "testsource");
-  ResStore store = s.resolvables();
-  MIL << "done reading source type " << s.type() << ": " << store <<  std::endl;
-  s.storeMetadata("./source-cache");
-  
-  std::list<Pathname> keys = s.publicKeys();
-  //Pathname root("."); 
-  //exit(1);
-  XMLFilesBackend backend(root);
-  backend.setFlag("locks", "name=bleh");
-  backend.setFlag("locks", "all except me");
-  
-  std::set<std::string> flags;
-      
-  flags = backend.flags("locks");
-  if (flags.size() != 2 )
-    ZYPP_THROW(Exception("wrote 2 flags, read != 2"));
-  
-  backend.removeFlag("locks", "all except me");
-  flags = backend.flags("locks");
-  if (flags.size() != 1 )
-    ZYPP_THROW(Exception("wrote 2 flags, deleted 1, read != 1"));
-  
-  
-  //backend.setRandomFileNameEnabled(true);
-  clock_t time_start, curr_time;
-  time_start = clock();
-
-  // now write the files
-  DBG << "Writing objects..." << std::endl;
-  for (ResStore::const_iterator it = store.begin(); it != store.end(); it++)
-  {
-    DBG << **it << endl;
-    backend.storeObject(*it);
-    //backend.setObjectFlag(*it, "blah1");
-    //backend.setObjectFlag(*it, "locked");
-    //backend.setObjectFlag(*it, "license-acepted");
-  }
-  
-  curr_time = clock() - time_start;           // time in micro seconds
-  MIL << "Wrote " << store.size() << " objects in " << (double) curr_time / CLOCKS_PER_SEC << " seconds" << std::endl;
-  
-  for ( ResStore::const_iterator it = store.begin(); it != store.end(); it++)
-  {
-    std::set<std::string> flags = backend.objectFlags(*it);
-    if ( flags.size() != 3 )
-    {
-      for ( std::set<std::string>::const_iterator itf = flags.begin(); itf != flags.end(); ++it)
-      {
-        //ERR << "Tag " << *itf << std::endl;
-      }
-      
-      //ERR << "Saved 3 tags, read " << flags.size() << " for " << **it << std::endl;
-      //ZYPP_THROW(Exception("Saved 3 tags, read other")); 
-    }
-  }
-  //exit(0);
-  time_start = clock();
-  std::list<ResObject::Ptr> objs = backend.storedObjects();
-  
-  curr_time = clock() - time_start;           // time in micro seconds
-  MIL << "Read " << objs.size() << " patches in " << (double) curr_time / CLOCKS_PER_SEC << " seconds" << std::endl;
-
-  INT << "===[SOURCES]==========================================" << endl;
-  PersistentStorage::SourceData data;
-  data.url = "http://localhost/rpms";
-  data.type = "yum";
-  data.alias = "duncan bugfree rpms";
-
-  backend.storeSource(data);
-
-  data.url = "http://localhost/debd";
-  data.type = "yum";
-  data.alias = "duncan bugfree 2";
-
-  backend.storeSource(data);
-
-  MIL << "Wrote 2 sources" << std::endl;
-  std::list<PersistentStorage::SourceData> sources = backend.storedSources();
-  MIL << "Read " << sources.size() << " sources" << std::endl;
-  return 0;
-}
 
 int nld10TestCase()
 {
@@ -423,7 +384,7 @@ int main()
     test4.named_flags_test();
     
     StorageTargetTest test5("./");
-    test5.publickeys_test();
+    test5.publickey_test();
     
     MIL << "store testsuite passed" << std::endl;
     return 0;
