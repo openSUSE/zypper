a8e461f73cfe7c3fe315b1dcff58059c1fcf9cbb
Michael Andres <ma@suse.de>
2006-03-18 01:44:09 +0000

    - drafted class NameKindProxy. Taking ResPool, name and kind, it collects all   PoolItems with this name and kind. Status queries and manipulators are to be   added.

---
+++ zypp/NameKindProxy.cc
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/NameKindProxy.cc
+ *
+*/
+#include <iostream>
+//#include "zypp/base/Logger.h"
+
+#include "zypp/NameKindProxy.h"
+
+using std::endl;
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+
+  namespace
+  {
+    /** \todo Move it to base/IOStream.h */
+    template<class _Tp>
+      struct PrintOn : public std::unary_function<_Tp, bool>
+      {
+        bool operator()( const _Tp & obj )
+        {
+          _str << _prfx << obj << std::endl;
+          return true;
+        }
+
+        PrintOn( std::ostream & str, const std::string & prfx = std::string() )
+        : _str( str )
+        , _prfx( prfx )
+        {}
+        std::ostream & _str;
+        std::string _prfx;
+      };
+  }
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  //	CLASS NAME : NameKindProxy
+  //
+  ///////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  //	METHOD NAME : NameKindProxy::NameKindProxy
+  //	METHOD TYPE : Ctor
+  //
+  NameKindProxy::NameKindProxy( ResPool pool_r, const std::string & name_r, Resolvable::Kind kind_r )
+    : _kind( kind_r )
+    , _name( name_r )
+    {
+      for ( ResPool::byName_iterator it = pool_r.byNameBegin( _name );
+            it != pool_r.byNameEnd( _name ); ++it )
+        {
+          if ( (*it)->kind() == _kind )
+            {
+              if ( it->status().isInstalled() )
+                _installed.insert( *it ) ;
+              else
+                _available.insert( *it );
+            }
+        }
+    }
+
+  /******************************************************************
+  **
+  **	FUNCTION NAME : operator<<
+  **	FUNCTION TYPE : std::ostream &
+  */
+  std::ostream & operator<<( std::ostream & str, const NameKindProxy & obj )
+  {
+    str << "[" << obj.kind() << "] " << obj.name()
+        << " {" << obj.installedSize() << "/" << obj.availableSize() << "}" << endl;
+    std::for_each( obj.installedBegin(), obj.installedEnd(), PrintOn<PoolItem>(str,"  ") );
+    std::for_each( obj.availableBegin(), obj.availableEnd(), PrintOn<PoolItem>(str,"  ") );
+    return str;
+  }
+
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
