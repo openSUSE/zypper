a538b46a88796a1cffef48aec1f6770dbea48e4f
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-03-20 19:37:45 +0000

    compile

--- testsuite/source/tests/ProductMetadataParserTest.cc
+++ testsuite/source/tests/ProductMetadataParserTest.cc
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include "zypp/Source.h"
+#include "zypp/Product.h"
 #include "zypp/source/susetags/ProductMetadataParser.h"
 #include "zypp/base/Logger.h"
 #include "zypp/base/LogControl.h"
@@ -10,22 +12,26 @@
 using namespace zypp;
 using namespace zypp::source::susetags;
 
+Product::Ptr parseContentFile(const Pathname &_content_file)
+{
+  ProductMetadataParser p;
+  p.parse( _content_file, Source_Ref::noSource );
+  return p.result;
+}
+
 int main()
 {
-  ProductMetadataParser parser;
   Product::Ptr product;
-
-  Source_Ref src;
 
   zypp::base::LogControl::instance().logfile( "-" );
 
   try {
-  product = parseContentFile(Pathname("products/content.1.txt"), src);
-  product = parseContentFile(Pathname("products/content.2.txt"), src);
-  product = parseContentFile(Pathname("products/content.3.txt"), src);
-  product = parseContentFile(Pathname("products/content.4.txt"), src);
+  product = parseContentFile(Pathname("products/content.1.txt"));
+  product = parseContentFile(Pathname("products/content.2.txt"));
+  product = parseContentFile(Pathname("products/content.3.txt"));
+  product = parseContentFile(Pathname("products/content.4.txt"));
   //DBG << "arch: " << entry.arch["x86_64"].size() << std::endl;
-  product = parseContentFile(Pathname("products/NOTTHERE.txt"), src);
+  product = parseContentFile(Pathname("products/NOTTHERE.txt"));
   //DBG << "arch: " << entry.arch["x86_64"].size() << std::endl;
   }
   catch (Exception & excpt_r) {
