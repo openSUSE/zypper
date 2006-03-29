273f2f467bb8ebfea1fd434378682e90bd725a9c
Michael Andres <ma@suse.de>
2006-03-29 21:06:11 +0000

    - added zypp/base/LogTools.h (ostream operator<< for vector/set/list,   printRange to print a range defined by iterators).

---
+++ zypp/base/LogTools.h
@@ -0,0 +1,79 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/base/LogTools.h
+ *
+*/
+#ifndef ZYPP_BASE_LOGTOOLS_H
+#define ZYPP_BASE_LOGTOOLS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include "zypp/base/Logger.h"
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+
+  /** Print range defined by iterators.
+   * \code
+   * intro [ pfx ITEM [ { sep ITEM }+ ] sfx ] extro
+   * \encode
+   * The defaults print the range enclosed in \c {}, one item per
+   * line indented by 2 spaces.
+   * \code
+   * {
+   *   item1
+   *   item2
+   * }
+   * {} // on empty rande
+   * \encode
+   * A comma separated list enclosed in \c () would be
+   * \code
+   * printRange( begin, end, stream, "(", "", ", ", "", ")" );
+   * \encode
+  */
+  template<class _Iterator>
+    std::ostream & printRange( _Iterator begin, _Iterator end,
+                               std::ostream & str,
+                               const std::string & intro = "{",
+                               const std::string & pfx   = "\n  ",
+                               const std::string & sep   = "\n  ",
+                               const std::string & sfx   = "\n",
+                               const std::string & extro = "}" )
+    {
+      str << intro;
+      if ( begin != end )
+        {
+          str << pfx << *begin;
+          for (  ++begin; begin != end; ++begin )
+            str << sep << *begin;
+          str << sfx;
+        }
+      return str << extro;
+    }
+
+  template<class _Tp>
+    std::ostream & operator<<( std::ostream & str, const std::vector<_Tp> & obj )
+    { return printRange( obj.begin(), obj.end(), str ); }
+
+  template<class _Tp>
+    std::ostream & operator<<( std::ostream & str, const std::set<_Tp> & obj )
+    { return printRange( obj.begin(), obj.end(), str ); }
+
+  template<class _Tp>
+    std::ostream & operator<<( std::ostream & str, const std::list<_Tp> & obj )
+    { return printRange( obj.begin(), obj.end(), str ); }
+
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
+#endif // ZYPP_BASE_LOGTOOLS_H
