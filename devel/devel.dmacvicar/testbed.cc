06636f5d7058c1e4bc0b1004d7339d02785ad4fb
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-04-24 11:36:00 +0000

    backup

---
+++ devel/devel.dmacvicar/testbed.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+
+#include "boost/filesystem/operations.hpp" // includes boost/filesystem/path.hpp
+#include "boost/filesystem/fstream.hpp"    // ditto
+
+#include <boost/iostreams/device/file_descriptor.hpp>
+
+#include <zypp/base/Logger.h>
+#include <zypp/Locale.h>
+#include <zypp/ZYpp.h>
+#include <zypp/ZYppFactory.h>
+#include <zypp/TranslatedText.h>
+///////////////////////////////////////////////////////////////////
+
+#include <zypp/parser/yum/YUMParser.h>
+#include <zypp/base/Logger.h>
+#include <zypp/NVRAD.h>
+#include <zypp/target/rpm/RpmDb.h>
+#include <zypp/source/yum/YUMScriptImpl.h>
+#include <zypp/source/yum/YUMMessageImpl.h>
+#include <zypp/source/yum/YUMPackageImpl.h>
+#include <zypp/source/yum/YUMSourceImpl.h>
+
+#include <map>
+#include <set>
+
+#include <zypp/CapFactory.h>
+#include <zypp/KeyRing.h>
+
+using namespace zypp::detail;
+
+using namespace std;
+using namespace zypp;
+using namespace zypp::parser::yum;
+using namespace zypp::source::yum;
+
+
+
+//using namespace DbXml;
+
+int main()
+{
+ 
+  NVRAD a("hola", Edition("2.0-3"));
+  NVRAD b("hola", Edition("2.0-3"));
+  
+  MIL << "a==b : " << (a==b) << std::endl;
+}
+
+
