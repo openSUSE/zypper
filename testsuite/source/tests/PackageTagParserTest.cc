9df0e7a79f64178230eebb6c04c0e8c49ed53b8f
Klaus Kaempf <kkaempf@suse.de>
2006-05-16 14:28:47 +0000

    some attempts to get this to compile, not successful

--- testsuite/source/tests/PackageTagParserTest.cc
+++ testsuite/source/tests/PackageTagParserTest.cc
@@ -1,17 +1,18 @@
 #include <iostream>
-#include "zypp/source/susetags/PackageTagFileParser.h"
+#include "zypp/source/susetags/PackagesParser.h"
 #include "zypp/target/store/serialize.h"
 #include "zypp/base/Logger.h"
 #include "zypp/base/LogControl.h"
 #include "zypp/base/Exception.h"
 #include "zypp/Pathname.h"
+#include "zypp/Source.h"
 
 using namespace std;
 using namespace zypp;
 
 void usage() {
-  std::cerr << "SelectionTagFileParserTest usage: "<< endl
-      << "SelectionTagFileParserTest file.sel" << endl;
+  std::cerr << "PackageTagFileParserTest usage: "<< endl
+      << "PackageTagFileParserTest file.sel" << endl;
 }
 
 int main(int argc, char **argv)
@@ -29,8 +30,8 @@ int main(int argc, char **argv)
 
   try
   {
-    selection = zypp::source::susetags::parseSelection( s, Pathname(argv[1]) );
-    cout << zypp::storage::toXML(selection) << endl;
+    package = zypp::source::susetags::parsePackages( s, Pathname(argv[1]) );
+    cout << zypp::storage::toXML(package) << endl;
   }
   catch (Exception & excpt_r)
   {
