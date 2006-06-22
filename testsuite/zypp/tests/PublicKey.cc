83bb4ca05155e155bb8f7dd2b7a182693faadbbb
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-06-22 14:23:30 +0000

    - testcases for PublicKey class

---
+++ testsuite/zypp/tests/PublicKey.cc
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <list>
+#include <string>
+
+#include "zypp/base/Logger.h"
+#include "zypp/base/Exception.h"
+#include "zypp/PublicKey.h"
+#include "zypp/ZYppFactory.h"
+#include "zypp/ZYpp.h"
+
+using namespace std;
+using namespace zypp;
+
+static void assert_equal( const std::string &got, const std::string &expected)
+{
+  if (got != expected)
+  {
+    ZYPP_THROW(Exception("Expected '" + expected + "', got '" + got + "'"));
+  }
+}
+
+int main( int argc, char * argv[] )
+{
+  try
+  {
+    zypp::devel::PublicKey k1("publickey-1.asc");
+  
+    assert_equal( k1.id(), "CD1EB6A9667E42D1");
+    assert_equal( k1.name(), "Duncan Mac-Vicar Prett <duncan@puc.cl>" );
+    assert_equal( k1.fingerprint(), "75DA7B971FD6ADB9A880BA9FCD1EB6A9667E42D1" );
+  
+    zypp::devel::PublicKey k2("publickey-suse.asc");
+  
+    assert_equal( k2.id(), "A84EDAE89C800ACA" );
+    assert_equal( k2.name(), "SuSE Package Signing Key <build@suse.de>" );
+    assert_equal( k2.fingerprint(), "79C179B2E1C820C1890F9994A84EDAE89C800ACA" );
+    
+    return 0;
+  }
+  catch(const Exception &e)
+  {
+    return 40;
+  }  
+  
+  return 0;
+}
