342832eaebcc3add523f09224999a1d6993244e2
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-06-30 13:51:26 +0000

    - PublicKey testcase to boost

--- testsuite/zypp/tests/PublicKey.cc
+++ testsuite/zypp/tests/PublicKey.cc
@@ -8,34 +8,38 @@
 #include "zypp/PublicKey.h"
 #include "zypp/ZYppFactory.h"
 #include "zypp/ZYpp.h"
-#include "testsuite/src/utils/TestUtils.h"
+
+#include <boost/test/unit_test.hpp>
+
+using boost::unit_test::test_suite;
+using boost::unit_test::test_case;
 
 using namespace std;
 using namespace zypp;
-using namespace zypp::testsuite::utils;
 
-int main( int argc, char * argv[] )
+void publickey_test()
 {
-  try
-  {
-    zypp::devel::PublicKey k1("publickey-1.asc");
+  BOOST_CHECK_THROW( zypp::devel::PublicKey("nonexistant"), Exception );
   
-    assert_equal( k1.id(), "CD1EB6A9667E42D1");
-    assert_equal( k1.name(), "Duncan Mac-Vicar Prett <duncan@puc.cl>" );
-    assert_equal( k1.fingerprint(), "75DA7B971FD6ADB9A880BA9FCD1EB6A9667E42D1" );
+  zypp::devel::PublicKey k1("publickey-1.asc");
   
-    zypp::devel::PublicKey k2("publickey-suse.asc");
+  BOOST_CHECK_EQUAL( k1.id(), "CD1EB6A9667E42D1");
+  BOOST_CHECK_EQUAL( k1.name(), "Duncan Mac-Vicar Prett <duncan@puc.cl>" );
+  BOOST_CHECK_EQUAL( k1.fingerprint(), "75DA7B971FD6ADB9A880BA9FCD1EB6A9667E42D1" );
   
-    assert_equal( k2.id(), "A84EDAE89C800ACA" );
-    assert_equal( k2.name(), "SuSE Package Signing Key <build@suse.de>" );
-    assert_equal( k2.fingerprint(), "79C179B2E1C820C1890F9994A84EDAE89C800ACA" );
+  zypp::devel::PublicKey k2("publickey-suse.asc");
+  
+  BOOST_CHECK_EQUAL( k2.id(), "A84EDAE89C800ACA" );
+  BOOST_CHECK_EQUAL( k2.name(), "SuSE Package Signing Key <build@suse.de>" );
+  BOOST_CHECK_EQUAL( k2.fingerprint(), "79C179B2E1C820C1890F9994A84EDAE89C800ACA" );
     
-    return 0;
-  }
-  catch(const Exception &e)
-  {
-    return 40;
-  }  
-  
-  return 0;
 }
+
+test_suite*
+init_unit_test_suite( int, char* [] )
+{
+    test_suite* test= BOOST_TEST_SUITE( "PublicKeyTest" );
+    test->add( BOOST_TEST_CASE( &publickey_test ), 0 /* expected zero error */ );
+    return test;
+}
+
