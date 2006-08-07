1deed7e28708d6743f4d71bed062f4317f72e304
Michael Andres <ma@suse.de>
2006-08-07 13:59:50 +0000

    changes

--- devel/devel.ma/Parse.cc
+++ devel/devel.ma/Parse.cc
@@ -109,12 +109,6 @@ struct PoolItemSelect
 };
 
 ///////////////////////////////////////////////////////////////////
-typedef std::list<PoolItem> PoolItemList;
-typedef std::set<PoolItem>  PoolItemSet;
-#include "zypp/solver/detail/InstallOrder.h"
-using zypp::solver::detail::InstallOrder;
-#include "Iorder.h"
-
 ///////////////////////////////////////////////////////////////////
 
 struct AddResolvables
@@ -181,10 +175,9 @@ int main( int argc, char * argv[] )
 {
   //zypp::base::LogControl::instance().logfile( "log.restrict" );
   INT << "===[START]==========================================" << endl;
-  return 0;
   ResPool pool( getZYpp()->pool() );
 
-  if ( 0 )
+  if ( 1 )
     {
       zypp::base::LogControl::TmpLineWriter shutUp;
       getZYpp()->initTarget( sysRoot );
@@ -196,10 +189,9 @@ int main( int argc, char * argv[] )
     //SourceManager::sourceManager()->restore( sysRoot );
     if ( 1 || SourceManager::sourceManager()->allSources().empty() )
       {
-        Source_Ref src1( createSource( "dir:///Local/SPBUG/GA" ) );
+        //Source_Ref src1( createSource( "dir:////schnell/CD-ARCHIVE/10.1/SUSE-Linux-10.1-RC5-FTP/CD1" ) );
+        Source_Ref src1( createSource( "ftp://schnell/CD-ARCHIVE/10.1/SUSE-Linux-10.1-RC5-FTP/CD1" ) );
         SourceManager::sourceManager()->addSource( src1 );
-        Source_Ref src2( createSource( "dir:///Local/SPBUG/SP" ) );
-        SourceManager::sourceManager()->addSource( src2 );
         //SourceManager::sourceManager()->store( sysRoot, true );
       }
     for_each( SourceManager::sourceManager()->Source_begin(), SourceManager::sourceManager()->Source_end(),
@@ -215,46 +207,14 @@ int main( int argc, char * argv[] )
 
   if ( 1 )
     {
-#if 0
-Resolver.cc(show_pool):915 1: U_Th_[S2:0][product]SUSE-Linux-Enterprise-Server-i386-10-0.i686
-2006-06-20 06:47:47 <0> linux(8525) [solver] Resolver.cc(show_pool):915 2: U_Tu_[S0:0][language]de_DE-.noarch
-2006-06-20 06:47:47 <0> linux(8525) [solver] Resolver.cc(show_pool):915 15: U_Th_[S2:0][pattern]x11-10-51.18.i586
-2006-06-20 06:47:47 <0> linux(8525) [solver] Resolver.cc(show_pool):915 60: U_Th_[S2:0][pattern]gnome-10-51.18.i586
-2006-06-20 06:47:47 <0> linux(8525) [solver] Resolver.cc(show_pool):915 67: U_Th_[S2:0][pattern]apparmor-10-51.18.i586
-2006-06-20 06:47:47 <0> linux(8525) [solver] Resolver.cc(show_pool):915 70: U_Th_[S2:0][pattern]print_server-10-51.18.i586
-2006-06-20 06:47:47 <0> linux(8525) [solver] Resolver.cc(show_pool):915 78: U_Th_[S2:0][pattern]base-10-51.18.i586
-2006-06-20 06:47:47 <0> linux(8525) [solver] Resolver.cc(show_pool):915 799: U_Th_[S2:1][package]kernel-default-2.6.16.20-0.12.i586
-2006-06-20 06:47:47 <0> linux(8525) [solver] Resolver.cc(show_pool):915 1989: U_Th_[S2:1][package]yast2-trans-de-2.13.24-0.2.noarch
-#endif
 #define selt(K,N) selectForTransact( nameKindProxy<K>( pool, #N ) )
-
-      selt( Language, de_DE );
-      selt( Language, de );
-      selt( Product,  SUSE-Linux-Enterprise-Server-i386 );
-      selt( Pattern,  x11 );
-      selt( Pattern,  gnome );
-      selt( Pattern,  apparmor );
-      selt( Pattern,  print_server );
-      selt( Pattern,  base );
-      selt( Package,  kernel-default );
-      selt( Package,  yast2-trans-de);
-      selectForTransact( nameKindProxy<Package>( pool, "glibc" ), Arch_i586 );
-    }
-  else
-    {
-      selectForTransact( nameKindProxy<Selection>( pool, "default" ) );
-      selectForTransact( nameKindProxy<Selection>( pool, "X11" ) );
-      selectForTransact( nameKindProxy<Selection>( pool, "Kde" ) );
-      selectForTransact( nameKindProxy<Selection>( pool, "Office" ) );
+      selt( Package,  bash );
+#undef selt
     }
 
   vdumpPoolStats( USR << "Transacting:"<< endl,
                   make_filter_begin<resfilter::ByTransact>(pool),
                   make_filter_end<resfilter::ByTransact>(pool) ) << endl;
-
-  //vdumpPoolStats( SEC,
-  //                pool.byKindBegin<Package>(),
-  //                pool.byKindEnd<Package>() ) << endl;
 
   if ( 1 ) {
     bool eres, rres;
@@ -272,43 +232,11 @@ Resolver.cc(show_pool):915 1: U_Th_[S2:0][product]SUSE-Linux-Enterprise-Server-i
                   make_filter_end<resfilter::ByTransact>(pool) ) << endl;
 
 
-  pool::GetResolvablesToInsDel collect( pool, pool::GetResolvablesToInsDel::ORDER_BY_MEDIANR );
-  MIL << "GetResolvablesToInsDel:" << endl << collect << endl;
-
   if ( 1 )
     {
-      // Collect until the 1st package from an unwanted media occurs.
-      // Further collection could violate install order.
-      bool hitUnwantedMedia = false;
-      PoolItemList::iterator fst=collect._toInstall.end();
-      for ( PoolItemList::iterator it = collect._toInstall.begin(); it != collect._toInstall.end(); ++it)
-        {
-          ResObject::constPtr res( it->resolvable() );
-
-          if ( hitUnwantedMedia
-               || ( res->sourceMediaNr() && res->sourceMediaNr() != 1 ) )
-            {
-              if ( !hitUnwantedMedia )
-                fst=it;
-              hitUnwantedMedia = true;
-            }
-          else
-            {
-            }
-        }
-      dumpRange( WAR << "toInstall1: " << endl,
-                 collect._toInstall.begin(), fst ) << endl;
-      dumpRange( WAR << "toInstall2: " << endl,
-                 fst, collect._toInstall.end() ) << endl;
-      dumpRange( ERR << "toDelete: " << endl,
-                 collect._toDelete.begin(), collect._toDelete.end() ) << endl;
-    }
-  else
-    {
-      dumpRange( WAR << "toInstall: " << endl,
-                 collect._toInstall.begin(), collect._toInstall.end() ) << endl;
-      dumpRange( ERR << "toDelete: " << endl,
-                 collect._toDelete.begin(), collect._toDelete.end() ) << endl;
+       ZYppCommitPolicy policy;
+       policy.rpmNoSignature();
+       ZYppCommitResult res( getZYpp()->commit( policy ) );
     }
 
   INT << "===[END]============================================" << endl << endl;
