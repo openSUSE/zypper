2a2b7a2af4068359ecf5f2c730396c8abd7537e2
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-08-18 13:51:00 +0000

    - start converting media testsuite to boost unit tests

---
+++ testsuite/media/media1.cc
@@ -0,0 +1,48 @@
+#include <zypp/media/MediaManager.h>
+#include <zypp/base/String.h>
+#include <zypp/base/Logger.h>
+#include <zypp/Pathname.h>
+
+#include <string>
+#include <list>
+#include <iostream>
+
+#include <boost/test/unit_test.hpp>
+
+#include "mymediaverifier.h"
+
+using namespace zypp;
+using namespace zypp::media;
+
+using boost::unit_test::test_suite;
+using boost::unit_test::test_case;
+
+void verifier_test()
+{
+  MediaVerifierRef verifier(
+      new MyMediaVerifier(/* "SUSE-Linux-CORE-i386 9" */)
+                           );
+  MediaManager     mm;
+  media::MediaId   id;
+
+  //id = mm.open(zypp::Url("cd:/"), "");
+  id = mm.open(zypp::Url("http://ftp.opensuse.org/pub/opensuse/distribution/SL-OSS-factory/inst-source/"), "");
+  mm.addVerifier( id, verifier);
+  mm.attach(id);
+  mm.provideFile(id, Pathname("/directory.yast"));
+  mm.release(id);
+  mm.attach(id);
+  mm.provideFile(id, Pathname("/directory.yast"));  
+}
+
+test_suite*
+init_unit_test_suite( int, char* [] )
+{
+  test_suite* test= BOOST_TEST_SUITE( "MediaVerifierTest" );
+  test->add( BOOST_TEST_CASE( &verifier_test ), 0 /* expected zero error */ );
+  return test;
+}
+
+
+
+// vim: set ts=2 sts=2 sw=2 ai et:
--- testsuite/media/media3.cc
+++ testsuite/media/media3.cc
@@ -10,61 +10,10 @@
 
 #include <signal.h>
 
+#include "mymediaverifier.h"
+
 using namespace zypp;
 using namespace zypp::media;
-
-/*
-** Very basic example verifier.
-**
-** This one does not know anything about the product, it
-** just checks if /media.1 (limited to 1st CD) exists...
-*/
-class MyMediaVerifier: public MediaVerifierBase
-{
-private:
-  // std::string _productname;
-public:
-  MyMediaVerifier(/* std::string &productname */)
-    : MediaVerifierBase()
-    //, _productname(productname)
-  {}
-
-  virtual
-  ~MyMediaVerifier()
-  {}
-
-  virtual bool
-  isDesiredMedia(const MediaAccessRef &ref)
-  {
-    DBG << "isDesiredMedia(): for media nr 1 " << std::endl;
-
-    if( !ref)
-      DBG << "isDesiredMedia(): invalid media handle" << std::endl;
-
-    std::list<std::string> lst;
-    Pathname               dir("/media.1");
-
-    DBG << "isDesiredMedia(): checking " << dir.asString() << std::endl;
-
-    // check the product e.g. via /media.1/products as well...
-    try
-    {
-      if( ref)
-        ref->dirInfo(lst, dir, false);
-    }
-    catch(const zypp::Exception &e)
-    {
-      ZYPP_CAUGHT(e);
-    }
-    DBG << "isDesiredMedia(): media "
-        << (lst.empty() ? "does not contain" : "contains")
-        << " the " << dir.asString() << " directory."
-        << std::endl;
-
-    return !lst.empty();
-  }
-};
-
 
 bool       do_step = false;
 int        do_quit = 0;
