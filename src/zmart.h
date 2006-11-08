dec0997aeaa5946a85cfa8283f240c9605b773c4
Jan Kupec <jkupec@suse.cz>
2006-11-08 19:07:48 +0000

    exit code numbers reassigned, man page updated

--- src/zmart.h
+++ src/zmart.h
@@ -32,16 +32,16 @@
 
 #define ZYPPER_EXIT_OK                     0
 // errors
-#define ZYPPER_EXIT_ERR_SYNTAX             1 // syntax error, e.g. zypper instal, zypper in --unknown option
-#define ZYPPER_EXIT_ERR_INVALID_ARGS       2 // invalid arguments given, e.g. zypper source-add httttps://asdf.net 
-#define ZYPPER_EXIT_ERR_ZYPP               3 // error indicated from within libzypp, e.g. God = zypp::getZYpp() threw an exception
-#define ZYPPER_EXIT_ERR_PRIVILEGES         4 // unsufficient privileges for the operation
-#define ZYPPER_EXIT_ERR_BUG                5 // undetermined error
+#define ZYPPER_EXIT_ERR_BUG                1 // undetermined error
+#define ZYPPER_EXIT_ERR_SYNTAX             2 // syntax error, e.g. zypper instal, zypper in --unknown option
+#define ZYPPER_EXIT_ERR_INVALID_ARGS       3 // invalid arguments given, e.g. zypper source-add httttps://asdf.net 
+#define ZYPPER_EXIT_ERR_ZYPP               4 // error indicated from within libzypp, e.g. God = zypp::getZYpp() threw an exception
+#define ZYPPER_EXIT_ERR_PRIVILEGES         5 // unsufficient privileges for the operation
 // info
-#define ZYPPER_EXIT_INF_UPDATE_NEEDED     -1 // update needed
-#define ZYPPER_EXIT_INF_SEC_UPDATE_NEEDED -2 // security update needed
-#define ZYPPER_EXIT_INF_REBOOT_NEEDED     -3 
-#define ZYPPER_EXIT_INF_RESTART_NEEDED    -4
+#define ZYPPER_EXIT_INF_UPDATE_NEEDED      100 // update needed
+#define ZYPPER_EXIT_INF_SEC_UPDATE_NEEDED  101 // security update needed
+#define ZYPPER_EXIT_INF_REBOOT_NEEDED      102 // reboot needed after install/upgrade 
+#define ZYPPER_EXIT_INF_RESTART_NEEDED     103 // restart of package manager itself needed
 
 struct Settings
 {
