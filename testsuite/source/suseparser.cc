7c5f13f44add8b3d3452e30c57cdb3d3ef8f00cb
Duncan Mac-Vicar P <dmacvicar@suse.de>
2006-11-27 16:03:16 +0000

    changes and backup

--- testsuite/source/suseparser.cc
+++ testsuite/source/suseparser.cc
@@ -29,6 +29,56 @@
 using namespace std;
 using namespace zypp;
 
+typedef std::map<std::string, unsigned int> CapabilityRank;
+CapabilityRank ranking;
+
+void print_rank()
+{
+  int counter_entries = 0;
+  int counter_caps = 0;
+  CapabilityRank::const_iterator it;
+  for ( it = ranking.begin(); it != ranking.end(); ++it )
+  {
+    std::cout << "\"" << (*it).first << "\"" << "," << (*it).second << std::endl;
+    counter_caps += (*it).second;
+    ++counter_entries;
+  }
+  
+  std::cout << "unique: " << counter_entries << " total: " << counter_caps << std::endl;
+}
+
+void rank( const CapSet &obj )
+{
+  for ( CapSet::const_iterator it = obj.begin(); it != obj.end(); ++it )
+  {
+    std::string capa((*it).asString());
+    CapabilityRank::const_iterator itr;
+    if ( ( itr = ranking.find( capa )) != ranking.end() )
+    {
+      ++ ranking[(*it).asString()];
+    }
+    else
+    {
+      ranking[(*it).asString()] = 1;
+    }
+  }
+}
+
+void rank( const Dependencies &obj )
+{
+    rank(obj[Dep::PROVIDES]);
+    rank(obj[Dep::PREREQUIRES]);
+    rank(obj[Dep::REQUIRES]);
+    rank(obj[Dep::CONFLICTS]);
+    rank(obj[Dep::OBSOLETES]);
+    rank(obj[Dep::RECOMMENDS]);
+    rank(obj[Dep::SUGGESTS]);
+    rank(obj[Dep::SUPPLEMENTS]);
+    rank(obj[Dep::ENHANCES]);
+    rank(obj[Dep::FRESHENS]);
+}
+
+
 /******************************************************************
 **
 **
@@ -44,7 +94,7 @@ int main( int argc, char * argv[] )
     cerr << "Usage: suseparse [--arch x] <susedir> [all|reallyall]" << endl;
     exit (1);
   }
-  zypp::base::LogControl::instance().logfile( "-" );
+  //zypp::base::LogControl::instance().logfile( "-" );
 
   INT << "===[START]==========================================" << endl;
 
@@ -156,7 +206,7 @@ int main( int argc, char * argv[] )
   for (ZYpp::LocaleSet::const_iterator it = locales.begin(); it != locales.end(); ++it)
   {
     if (it != locales.begin()) std::cout << ", ";
-    std::cout << it->code();
+    //std::cout << it->code();
   }
   std::cout << endl;
 
@@ -166,19 +216,18 @@ int main( int argc, char * argv[] )
     for (ResStore::iterator it = store.begin(); it != store.end(); ++it)
     {
       ResObject::constPtr robj = *it;
-      std::cout << ++count << *robj << endl;
+      //std::cout << ++count << *robj << endl;
       if (strcmp(argv[argpos], "reallyall") == 0)
       {
         const Dependencies & deps = robj->deps();
-        std::cout << deps << endl;
-        if (isKind<Package>( robj ))
-        {
-          Package::constPtr pkg( asKind<Package>( robj ) );
-          std::cout << "License[" << pkg->licenseToConfirm() << "]" << endl;
-        }
+        
+        rank(deps);
+        
+        //std::cout << deps << endl;
+        
       }
     }
-
+    print_rank();
   }
   else
   {
