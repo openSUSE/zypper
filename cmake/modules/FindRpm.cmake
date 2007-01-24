281f6dc713a56afbda526cdca59156631ae265a1
Duncan Mac-Vicar P <dmacvicar@suse.de>
2007-01-24 16:49:39 +0000

    cmake updates and finders

---
+++ cmake/modules/FindRpm.cmake
@@ -0,0 +1,28 @@
+
+if(RPM_INCLUDE_DIR AND RPM_LIBRARY)
+	# Already in cache, be silent
+	set(RPM_FIND_QUIETLY TRUE)	
+endif(RPM_INCLUDE_DIR AND RPM_LIBRARY)
+
+set(RPM_LIBRARY)
+set(RPM_INCLUDE_DIR)
+
+FIND_PATH(RPM_INCLUDE_DIR rpm/misc.h
+	/usr/include
+	/usr/local/include
+)
+
+FIND_LIBRARY(RPM_LIBRARY NAMES rpm
+	PATHS
+	/usr/lib
+	/usr/local/lib
+)
+
+if(RPM_INCLUDE_DIR AND RPM_LIBRARY)
+   MESSAGE( STATUS "rpm found: includes in ${RPM_INCLUDE_DIR}, library in ${RPM_LIBRARY}")
+   set(RPM_FOUND TRUE)
+else(RPM_INCLUDE_DIR AND RPM_LIBRARY)
+   MESSAGE( STATUS "rpm not found")
+endif(RPM_INCLUDE_DIR AND RPM_LIBRARY)
+
+MARK_AS_ADVANCED(RPM_INCLUDE_DIR RPM_LIBRARY)
