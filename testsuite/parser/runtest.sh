7d3d14c6b5025204eb52e3ac49e8dea653a9af12
Stefan Haas <shaas@suse.de>
2007-02-15 12:40:07 +0000

    outsourcing libzypp

--- testsuite/parser/runtest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# goes through each testcase in this directory
-# see README
-testcase=$1;
-mode="${testcase#*-}"
-mode="${mode%%-*}"
-
-./YUMtest "$mode" < "$testcase"
-echo "Exit Code: $?"
--- testsuite/solver/data.deptestomatic/yast-tests/makesolution.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-test=$1
-base=`basename $1 .xml`
-../../testsuite/deptestomatic $test | grep \>\!\> > $base.solution
