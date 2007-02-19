1b5f2ada5389a1682fb471c7c486c41d2e347b12
Duncan Mac-Vicar P <dmacvicar@suse.de>
2007-02-19 15:22:20 +0000

    missing

---
+++ libzypp.pc.cmake
@@ -0,0 +1,14 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@LIB_INSTALL_DIR@/zypp
+libdir=@LIB_INSTALL_DIR@
+includedir=@CMAKE_INSTALL_PREFIX@/include
+
+Name: @PACKAGE@
+Version: @VERSION@
+Description: Package, Patch, Pattern, and Product Management
+
+Libs: -L${libdir} -lzypp
+Cflags: -I${includedir} @ZYPP_CFLAGS@
+
+features=@ZYPP_FEATURES@
+
