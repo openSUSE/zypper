245cf9012f6a7f047b777a0e7a8dbda4867adef3
Duncan Mac-Vicar P <dmacvicar@suse.de>
2007-04-23 11:30:20 +0000

    backup

---
+++ devel/devel.dmacvicar/SourceManager.cc
@@ -0,0 +1,3 @@
+
+#include "SourceManager.h"
+
--- devel/devel.dmacvicar/SourceManager.h
+++ devel/devel.dmacvicar/SourceManager.h
@@ -1,3 +1,19 @@
 
 #ifndef ZYPP_NEW_SOURCEMANAGER_H
 #define ZYPP_NEW_SOURCEMANAGER_H
+
+#include "zypp/base/ReferenceCounted.h"
+#include "zypp/base/NonCopyable.h"
+#include "zypp/Pathname.h"
+
+namespace zypp2
+{
+  
+  class SourceManager : public base::ReferenceCounted, private base::NonCopyable
+  {
+    friend std::ostream & operator<<( std::ostream & str, const SourceManager & obj );
+  };
+  
+  
+}
+#endif
