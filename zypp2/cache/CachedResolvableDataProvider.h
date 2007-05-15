145f7f59be4dce59fe8c63e2cd2e471913df46ca
Michael Andres <ma@suse.de>
2007-05-15 16:27:04 +0000

    updated data:: resolvable classes.

--- zypp2/cache/CachedResolvableDataProvider.h
+++ zypp2/cache/CachedResolvableDataProvider.h
@@ -12,55 +12,54 @@ namespace zypp
 {
   namespace cache
   {
-    
+
     template<class _res> class CachedDataQuery;
-    
+
     template<> class CachedDataQuery<Package>
     {
-      
+
     };
-    
+
     template<class _Res> class DataProviderRestraits
     {
       public:
-      typedef data::SpecificData<_Res> DataType;
-      
+
     };
-    
+
     template<class _Res>
     class CachedDataProvider
     {
       CachedDataProvider<_Res>( const zypp::Pathname &dbdir, const data::RecordId id );
     };
-    
+
 //     template<> class CachedDataProvider<ResObject> : public DataProviderRestraits<ResObject>::DataType
 //     {
 //       public:
 //       CachedDataProvider<Package>( sqlite::sqlite3_connection &con, const data::RecordId id )
 //       {
-//         //sqlite3_command( con, "select 
+//         //sqlite3_command( con, "select
 //         //con.
 //       }
-//       
+//
 //       private:
 //       //DataType
 //     };
-    
+
 //     template<> class CachedDataProvider<Package> : public DataProviderRestraits<Package>::DataType
 //     {
 //       public:
 //       CachedDataProvider<Package>( sqlite3x::sqlite3_connection &con, const data::RecordId id )
 //       {
-//         //sqlite3_command( con, "select 
+//         //sqlite3_command( con, "select
 //         //con.
 //       }
 //     };
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
   }
 }
 
