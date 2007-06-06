9a94aa04a6047dabc98d837fb2b32a164263a0c3
Duncan Mac-Vicar P <dmacvicar@suse.de>
2007-06-06 11:58:17 +0000

    Not needed

--- zypp2/cache/schema/schema_filelist3.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS file_entries;
-
-CREATE TABLE file_entries (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-  , package_id INTEGER
-  , dir TEXT UNIQUE
-  , files TEXT
-);
