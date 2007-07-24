8cd53f2c56c723b5198361f9d7d25a9fa079e81d
Duncan Mac-Vicar P <dmacvicar@suse.de>
2007-07-24 12:35:00 +0000

    - ups, compile

--- zypp/repo/RepoProvideFile.cc
+++ zypp/repo/RepoProvideFile.cc
@@ -138,8 +138,6 @@ namespace zypp
         }
       } // iteration over urls
 
-      stringstream ss;
-      ss << 
       ZYPP_THROW(Exception(str::form(_("Can't provide file %s from repository %s",
                                        loc_r.filename().c_str(),
                                        info.alias().c_str() ) ) ) );
