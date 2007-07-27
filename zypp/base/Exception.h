f233e7c1d2abcca1835da6db328262368cda3348
Michael Andres <ma@suse.de>
2007-07-27 12:15:16 +0000

    docs

--- zypp/base/Exception.h
+++ zypp/base/Exception.h
@@ -106,12 +106,37 @@ namespace zypp
    *  Main.cc(main):57 CAUGHT:   Main.cc(main):51: Something bad happened.
    * \endcode
    *
+   *
+   * Class Exception now offers a history list of message strings.
+   * These messages should describe what lead to the exception.
+   *
+   * The Exceptions message itself is NOT included in the history.
+   *
+   * Rethrow, remembering an old exception:
+   * \code
+   * try
+   * {
+   *   ....
+   * }
+   * catch( const Exception & olderr_r )
+   * {
+   *    ZYPP_CAUGHT( olderr_r )
+   *    HighLevelException newerr( "Something failed." );
+   *    newerr.rember( olderr_r );
+   *    ZYPP_THROW( newerr );
+   * }
+   * \endcode
+   *
+   * Print an Exception followed by it's history if available:
+   * \code
+   * Exception error;
+   * ERR << error << endl << error.historyAsString();
+   * \endcode
+   *
    * \todo That's a draft to have a common way of throwing exceptions.
    * Most probabely we'll finally use blocxx exceptions. Here, but not
    * in the remaining code of zypp. If we can we should try to wrap
    * the blocxx macros and typedef the classes in here.
-   *
-   * \todo maybe location and message stack.
    **/
   class Exception : public std::exception
   {
