be46f5a7bb58d11735c40f5c81b2ba451649baf1
Stefan Schubert <schubi@suse.de>
2007-07-31 08:34:52 +0000

    logging not needed

--- zypp/solver/detail/ResolverInfoContainer.cc
+++ zypp/solver/detail/ResolverInfoContainer.cc
@@ -23,7 +23,6 @@
 #include <sstream>
 
 #include "zypp/solver/detail/ResolverInfoContainer.h"
-#include "zypp/base/Logger.h"
 #include "zypp/solver/detail/Helper.h"
 
 /////////////////////////////////////////////////////////////////////////
@@ -172,7 +171,6 @@ ResolverInfoContainer::mentions (PoolItem_Ref item) const
     // Search item_list for any mention of the item.
 
     for (PoolItemList::const_iterator iter = _item_list.begin(); iter != _item_list.end(); iter++) {
-	MIL << "yyyyyyxx " << item <<  " xx " << *iter << endl;	
 	if ((*iter)->name() == item->name()) {
 	    return true;
 	}
