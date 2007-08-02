1d2873a75dc9c8fa58e8ef5c93707e216904bbbf
Thomas Goettlicher <tgoettlicher@suse.de>
2007-08-02 14:09:50 +0000

    added xml error message to zypp-checkpatches-wrapper

--- src/zypp-checkpatches-wrapper.c
+++ src/zypp-checkpatches-wrapper.c
@@ -32,6 +32,12 @@ int main (void) {
     if (setuid (0)) {
 	perror ("setuid");
 	fprintf (stderr, "Forgot to chmod this program?\n");
+	fprintf (stdout, "<?xml version='1.0'?>\n");
+	fprintf (stdout, "<update-status version=\"0.5\">\n");
+	fprintf (stdout, " <errors>");
+	fprintf (stdout, "  <error>Unable to check for patches and updates because /usr/sbin/zypp-checkpatches-wrapper helper programm is not set SUID root.\nThis problem might be solved by setting \"File Permissons\" in YaST \"Local Security\" tab to \"easy\" or by modifying /etc/permissions.local</error>");
+	fprintf (stdout, " </errors>");
+	fprintf (stdout, " </update-status>");
 	return WRAPPER_ERROR;
     }
  
