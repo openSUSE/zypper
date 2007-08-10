17269dbabd626d6c7467966ae84b1cc5e562d02b
Duncan Mac-Vicar P <dmacvicar@suse.de>
2007-08-10 14:08:35 +0000

    zypp uses 2 space indentation

--- zypp/base/Regex.h
+++ zypp/base/Regex.h
@@ -30,34 +30,33 @@ namespace zypp
     class regex {
     public:
 
-        enum RegFlags {
-            optimize = 0,
-            match_extra = 0,
-            icase = REG_ICASE,
-            nosubs = REG_NOSUB,
-            match_extended = REG_EXTENDED
-        };
+      enum RegFlags {
+        optimize = 0,
+        match_extra = 0,
+        icase = REG_ICASE,
+        nosubs = REG_NOSUB,
+        match_extended = REG_EXTENDED
+      };
 
-        regex(const std::string& s,int flags = match_extended);
-        ~regex() throw();
-
+      regex(const std::string& s,int flags = match_extended);
+      ~regex() throw();
 
     public:
-        regex_t m_preg;
-        bool m_valid;
+      regex_t m_preg;
+      bool m_valid;
     };
 
 
     class smatch {
     public:
-        smatch();
+      smatch();
 
-        std::string operator[](unsigned i) const;
+      std::string operator[](unsigned i) const;
 
-        unsigned size() const;
+      unsigned size() const;
 
-        std::string match_str;
-        regmatch_t pmatch[12];
+      std::string match_str;
+      regmatch_t pmatch[12];
     };
 
     bool regex_match(const std::string& s, str::smatch& matches, const regex& regex);
