d0190141cd7961e346a2b7a7e3ea74dd63124b87
Klaus Kaempf <kkaempf@suse.de>
2007-08-29 14:33:00 +0000

    - the media.1/media uniquely identifies a 'susetags' repo, not   the content file (#304200)

--- zypp/RepoManager.cc
+++ zypp/RepoManager.cc
@@ -327,7 +327,7 @@ namespace zypp
 
       case RepoType::YAST2_e :
       {
-        status = RepoStatus( rawpath + "/content");
+        status = RepoStatus( rawpath + "/media.1/media");
       }
       break;
 
