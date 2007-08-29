553cc9385b4cc122f0b1a2ef12e1d964f2269457
Stefan Schubert <schubi@suse.de>
2007-08-29 11:36:24 +0000

    Add a new solver solution in the case of running in a timeout: ProblemSolutionDoubleTimeout.h #Bug 302496

---
+++ zypp/solver/detail/ProblemSolutionDoubleTimeout.cc
@@ -0,0 +1,64 @@
+
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* ProblemSolution.cc
+ *
+ * Easy-to use interface to the ZYPP dependency resolver
+ *
+ * Copyright (C) 2000-2002 Ximian, Inc.
+ * Copyright (C) 2005 SUSE Linux Products GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include <sstream>
+
+#include "zypp/base/String.h"
+#include "zypp/base/Gettext.h"
+#include "zypp/solver/detail/ProblemSolutionDoubleTimeout.h"
+
+using namespace std;
+
+/////////////////////////////////////////////////////////////////////////
+namespace zypp
+{ ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  namespace solver
+  { /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+    namespace detail
+    { ///////////////////////////////////////////////////////////////////
+
+IMPL_PTR_TYPE(ProblemSolutionDoubleTimeout);
+
+//---------------------------------------------------------------------------
+
+ProblemSolutionDoubleTimeout::ProblemSolutionDoubleTimeout( ResolverProblem_Ptr parent)
+    : ProblemSolution (parent, "", "")
+{
+    _description = _("Double timeout");
+    _details = _("Start the next solver run with doubled timeout.");
+
+    addAction ( new TransactionSolutionAction (DOUBLETIMEOUT));
+}
+
+      ///////////////////////////////////////////////////////////////////
+    };// namespace detail
+    /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+  };// namespace solver
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+};// namespace zypp
+/////////////////////////////////////////////////////////////////////////
