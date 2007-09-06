a5f3cd4439c2cef6ed8a49d1f459a5ca63f99e29
Michael Andres <ma@suse.de>
2007-09-06 12:30:02 +0000

    add newline at eof

--- tests/zypp/TranslatedText.cc
+++ tests/zypp/TranslatedText.cc
@@ -26,32 +26,32 @@ using namespace zypp;
 void test_tt()
 {
   ZYpp::Ptr god;
-  
+
   god = getZYpp();
-  
+
   TranslatedText testTT;
   MIL << "Locale: en" << std::endl;
   god->setTextLocale(Locale("en"));
   testTT.setText("default");
   MIL << "value: '" << testTT.text() << "'" << std::endl;
   BOOST_CHECK_EQUAL( testTT.text(), "default" );
-  
+
   testTT.setText("default english", Locale("en"));
   BOOST_CHECK_EQUAL( testTT.text(), "default english" );
-  
+
   MIL << "Locale: es_ES" << std::endl;
   god->setTextLocale(Locale("es_ES"));
-  
+
   BOOST_CHECK_EQUAL( testTT.text(), "default english" );
-    
+
   testTT.setText("hola esto es neutro", Locale("es"));
   testTT.setText("this is neutral", Locale("en"));
-  
+
   BOOST_CHECK_EQUAL( testTT.text(), "hola esto es neutro" );
-    
+
   testTT.setText("hola Spain", Locale("es_ES"));
   BOOST_CHECK_EQUAL( testTT.text(), "hola Spain" );
-  
+
   MIL << "Locale: null" << std::endl;
   god->setTextLocale(Locale());
   BOOST_CHECK_EQUAL( testTT.text(), "default" );
