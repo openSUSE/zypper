f8a31acbb792e1f9e16841003061ce2c58c63458
Stefan Haas <shaas@suse.de>
2007-09-07 10:17:31 +0000

    changed CMakeLists.txt

--- devel/devel.shaas/PdbToZypp/pdbtozypp.cc
+++ devel/devel.shaas/PdbToZypp/pdbtozypp.cc
@@ -16,8 +16,8 @@ void PdbToZypp::readOut(){
 
    //store = new ResStore;
 
-	database *dbDeps = new database("lorien.suse.de", "rpmread", "Salahm1", "rpm");
-	database *dbPackages = new database("lorien.suse.de", "rpmread", "Salahm1", "package");
+	database *dbDeps = new database("lorien.suse.de", "rpmread", "rrrrrrr", "rpm");
+	database *dbPackages = new database("lorien.suse.de", "rpmread", "rrrrrrr", "package");
 
 	if(dbPackages->connect() != 1){
 		//std::cout << "NO DB CONNECTION!!!\n";
