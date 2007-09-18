1648b79eb7c0a01b6408115ffab6579ce507427e
Arvin Schnell <aschnell@suse.de>
2007-09-18 15:50:18 +0000

    - call setgid and umask in zypp-checkpatches-wrapper (bug #325556)

--- src/zypp-checkpatches-wrapper.c
+++ src/zypp-checkpatches-wrapper.c
@@ -1,5 +1,9 @@
 /* A setuid-root wrapper for zypper refresh repositories */
 
+/* setgid and umask */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <grp.h>
 /* clearenv */
 #include <stdlib.h>
 /* chdir, execl, setuid */
@@ -29,7 +33,16 @@ int main (void) {
     }
     /* set minimal environment... done */
     /* prevent the user from sending signals */
-    if (setuid (0)) {
+
+    if (initgroups("root", 0) != 0 || setgid (0) != 0) {
+	fprintf (stdout, "<?xml version='1.0'?>\n");
+	fprintf (stdout, "<stream>\n");
+	fprintf (stdout, "<message type=\"error\">Unable to check for patches and updates because /usr/sbin/zypp-checkpatches-wrapper helper programm is not set SUID root.\nThis problem might be solved by setting \"File Permissons\" in YaST \"Local Security\" tab to \"easy\" or by modifying /etc/permissions.local</message>\n");
+	fprintf (stdout, "</stream>\n");
+	return WRAPPER_ERROR;
+    }
+
+    if (setuid (0) != 0) {
 //	perror ("setuid");
 //	fprintf (stderr, "Forgot to chmod this program?\n");
 	fprintf (stdout, "<?xml version='1.0'?>\n");
@@ -38,7 +51,9 @@ int main (void) {
 	fprintf (stdout, "</stream>\n");
 	return WRAPPER_ERROR;
     }
- 
+
+    umask(0022);
+
     /* execute the real application */
     execl (app, app, arg1, arg2, arg3, arg4, (char *) NULL);
 
