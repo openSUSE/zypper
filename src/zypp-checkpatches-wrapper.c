4f3dba9eafd82b39a323e7385ab5dfff85896d1b
Arvin Schnell <aschnell@suse.de>
2007-09-24 07:57:06 +0000

    - close file-descriptors > 2, call setgid and umask and check   file-descriptors 0, 1 and 2 in zypp-checkpatches-wrapper (bug   #325556)

--- src/zypp-checkpatches-wrapper.c
+++ src/zypp-checkpatches-wrapper.c
@@ -1,8 +1,13 @@
 /* A setuid-root wrapper for zypp-checkpatches */
 
+/* setgid, umask and open */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <grp.h>
+#include <fcntl.h>
 /* clearenv */
 #include <stdlib.h>
-/* chdir, execl, setuid */
+/* chdir, execl, setuid, exit */
 #include <unistd.h>
 /* perror */
 #include <stdio.h>
@@ -11,7 +16,24 @@
 
 const char *app = "/usr/sbin/zypp-checkpatches";
 
-int main (void) {
+int main (void)
+{
+    /* see http://rechner.lst.de/~okir/blackhats/node41.html */
+    while (1) {
+	int fd = open("/dev/null", O_RDWR);
+	if (fd < 0)
+	    return WRAPPER_ERROR;
+	if (fd > 2) {
+	    close(fd);
+	    break;
+	}
+    }
+
+    /* see http://rechner.lst.de/~okir/blackhats/node35.html */
+    int fd = getdtablesize();
+    while (fd-- > 2)
+        close(fd);
+
     /* cd / to avoid NFS problems */
     if (chdir ("/")) {
 	perror ("chdir");
@@ -25,12 +47,21 @@ int main (void) {
     }
     /* set minimal environment... done */
     /* prevent the user from sending signals */
+
+    if (initgroups("root", 0) != 0 || setgid (0) != 0) {
+    	perror ("setuid");
+	fprintf (stderr, "Forgot to chmod this program?\n");
+	return WRAPPER_ERROR;
+    }
+
     if (setuid (0)) {
 	perror ("setuid");
 	fprintf (stderr, "Forgot to chmod this program?\n");
 	return WRAPPER_ERROR;
     }
- 
+
+    umask(0022);
+
     /* execute the real application */
     execl (app, app, (char *) NULL);
 
