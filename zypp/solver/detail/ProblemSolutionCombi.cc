ba7749a67b6412cbcf5921b668beff2132baab60
Stefan Schubert <schubi@suse.de>
2007-11-07 16:30:20 +0000

    Added a Solution with different actions. (Needed for SAT-Solver)

---
+++ zypp/solver/detail/ProblemSolutionCombi.cc
@@ -0,0 +1,73 @@
+
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* ProblemSolution.cc
+ *
+ * Easy-to use interface to the ZYPP dependency resolver
+ *
+ * Copyright (C) 2000-2002 Ximian, Inc.
+ * Copyright (C) 2005 SUSE Linux Products GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include <sstream>
+
+#include "zypp/base/String.h"
+#include "zypp/base/Gettext.h"
+
+#include "zypp/solver/detail/ProblemSolutionCombi.h"
+#include "zypp/solver/detail/ResolverInfo.h"
+
+using namespace std;
+
+/////////////////////////////////////////////////////////////////////////
+namespace zypp
+{ ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  namespace solver
+  { /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+    namespace detail
+    { ///////////////////////////////////////////////////////////////////
+
+IMPL_PTR_TYPE(ProblemSolutionCombi);
+
+//---------------------------------------------------------------------------
+
+ProblemSolutionCombi::ProblemSolutionCombi( ResolverProblem_Ptr parent)
+    : ProblemSolution (parent, "", "")
+{
+    _description = "";
+    _details = "";
+}
+
+void ProblemSolutionCombi::addSingleAction( PoolItem_Ref item, const TransactionKind action)
+{
+    addAction ( new TransactionSolutionAction (item, REMOVE));
+
+    ostringstream details;
+    details << _actions;    
+    _description += details.str();
+}
+
+      ///////////////////////////////////////////////////////////////////
+    };// namespace detail
+    /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+  };// namespace solver
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+};// namespace zypp
+/////////////////////////////////////////////////////////////////////////
---
+++ zypp/solver/detail/ProblemSolutionCombi.h
@@ -0,0 +1,69 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* Resolver_problems.cc
+ *
+ * Copyright (C) 2000-2002 Ximian, Inc.
+ * Copyright (C) 2005 SUSE Linux Products GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef ZYPP_SOLVER_DETAIL_PROBLEMSOLUTIONINSTALL_H
+#define ZYPP_SOLVER_DETAIL_PROBLEMSOLUTIONINSTALL_H
+
+#include "zypp/ProblemSolution.h"
+#include "zypp/solver/detail/Types.h"
+
+/////////////////////////////////////////////////////////////////////////
+namespace zypp
+{ ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  namespace solver
+  { /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+    namespace detail
+    { ///////////////////////////////////////////////////////////////////
+
+	/**
+	 * Class representing one possible solution to one problem found during resolving
+	 * This problem solution is a combination of different actions.
+	 * e.G. install, delete, keep different resolvables.
+	 * 
+	 **/
+	class ProblemSolutionCombi : public ProblemSolution
+	{
+	public:
+
+	    /**
+	     * Constructor.
+	     **/
+	    ProblemSolutionCombi( ResolverProblem_Ptr parent );
+	    /**
+	     * Add a single action
+	     */
+	    void addSingleAction( PoolItem_Ref item, const TransactionKind action);
+	};
+
+      ///////////////////////////////////////////////////////////////////
+    };// namespace detail
+    /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+  };// namespace solver
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+};// namespace zypp
+/////////////////////////////////////////////////////////////////////////
+
+#endif // ZYPP_SOLVER_DETAIL_PROBLEMSOLUTIONAINSTALL_H
+
