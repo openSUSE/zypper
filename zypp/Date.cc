8eaddcc9b7112dc69bd64da678ebcfbd6f9eed05
Michael Andres <ma@suse.de>
2007-11-14 17:26:05 +0000

    solution without static strings

--- zypp/Date.cc
+++ zypp/Date.cc
@@ -39,55 +39,41 @@ namespace zypp
   {
     static char buf[1024];
 
-    static std::string lastLocale;
-    static std::string needLocale;
     const char * tmp = ::setlocale( LC_TIME, NULL );
     std::string thisLocale( tmp ? tmp : "" );
 
-    if ( thisLocale != lastLocale )
+    if (    thisLocale.find( "UTF-8" ) == std::string::npos
+         && thisLocale.find( "utf-8" ) == std::string::npos
+         && thisLocale != "POSIX"
+         && thisLocale != "C"
+         && thisLocale != "" )
     {
-      // locale changes since last call. compute new utf-8 locale
-      if (    thisLocale.find( "UTF-8" ) != std::string::npos
-           || thisLocale.find( "utf-8" ) != std::string::npos
-           || thisLocale == "POSIX"
-           || thisLocale == "C"
-           || thisLocale == "" )
+      // language[_territory][.codeset][@modifier]
+      // add/exchange codeset with UTF-8
+      std::string needLocale = ".UTF-8";
+      std::string::size_type loc = thisLocale.find_first_of( ".@" );
+      if ( loc != std::string::npos )
       {
-        // is UTF-8 or C or POSIX
-        needLocale = thisLocale;
+        // prepend language[_territory]
+        needLocale = thisLocale.substr( 0, loc ) + needLocale;
+        loc = thisLocale.find_last_of( "@" );
+        if ( loc != std::string::npos )
+        {
+          // append [@modifier]
+          needLocale += thisLocale.substr( loc );
+        }
       }
       else
       {
-        // language[_territory][.codeset][@modifier]
-        // add/exchange codeset with UTF-8
-        needLocale = ".UTF-8";
-        std::string::size_type loc = thisLocale.find_first_of( ".@" );
-        if ( loc != std::string::npos )
-        {
-          // prepend language[_territory]
-          needLocale = thisLocale.substr( 0, loc ) + needLocale;
-          loc = thisLocale.find_last_of( "@" );
-          if ( loc != std::string::npos )
-          {
-            // append [@modifier]
-            needLocale += thisLocale.substr( loc );
-          }
-        }
-        else
-        {
-          // append ".UTF-8"
-          needLocale = thisLocale + needLocale;
-        }
+        // append ".UTF-8"
+        needLocale = thisLocale + needLocale;
       }
-      lastLocale = thisLocale;
+      ::setlocale( LC_TIME, needLocale.c_str() );
     }
 
-    if ( needLocale != lastLocale )
-      ::setlocale( LC_TIME, needLocale.c_str() );
     if ( ! strftime( buf, 1024, format_r.c_str(), localtime( &_date ) ) )
       *buf = '\0';
-    if ( needLocale != lastLocale )
-      ::setlocale( LC_TIME, lastLocale.c_str() );
+
     return buf;
   }
 
