881e96d7a405897fdc47791ec12aeef2f711608d
Michael Andres <ma@suse.de>
2007-11-14 13:26:04 +0000

    Output date strings in UTF-8.

--- zypp/Date.cc
+++ zypp/Date.cc
@@ -38,8 +38,56 @@ namespace zypp
   std::string Date::form( const std::string & format_r ) const
   {
     static char buf[1024];
+
+    static std::string lastLocale;
+    static std::string needLocale;
+    const char * tmp = ::setlocale( LC_TIME, NULL );
+    std::string thisLocale( tmp ? tmp : "" );
+
+    if ( thisLocale != lastLocale )
+    {
+      // locale changes since last call. compute new utf-8 locale
+      if (    thisLocale.find( "UTF-8" ) != std::string::npos
+           || thisLocale.find( "utf-8" ) != std::string::npos
+           || thisLocale == "POSIX"
+           || thisLocale == "C"
+           || thisLocale == "" )
+      {
+        // is UTF-8 or C or POSIX
+        needLocale = thisLocale;
+      }
+      else
+      {
+        // language[_territory][.codeset][@modifier]
+        // add/exchange codeset with UTF-8
+        needLocale = ".UTF-8";
+        std::string::size_type loc = thisLocale.find_first_of( ".@" );
+        if ( loc != std::string::npos )
+        {
+          // prepend language[_territory]
+          needLocale = thisLocale.substr( 0, loc ) + needLocale;
+          loc = thisLocale.find_last_of( "@" );
+          if ( loc != std::string::npos )
+          {
+            // append [@modifier]
+            needLocale += thisLocale.substr( loc );
+          }
+        }
+        else
+        {
+          // append ".UTF-8"
+          needLocale = thisLocale + needLocale;
+        }
+      }
+      lastLocale = thisLocale;
+    }
+
+    if ( needLocale != lastLocale )
+      ::setlocale( LC_TIME, needLocale.c_str() );
     if ( ! strftime( buf, 1024, format_r.c_str(), localtime( &_date ) ) )
-      return std::string();
+      *buf = '\0';
+    if ( needLocale != lastLocale )
+      ::setlocale( LC_TIME, lastLocale.c_str() );
     return buf;
   }
 
