2f21db99632ed78776987b9ff6e78e1d73046e9b
Michael Matz <matz@suse.de>
2008-02-11 04:30:08 +0000

    Dear Lord!  There's always infinite amazement into which total and utter crap code can transform by using boost.  This removes 325 lines, adds 47 new ones (for a slow implementation of XML escaping) and now my "zypper in kde4-bozo" takes 14 seconds instead of 128.  Yes, nine times faster!  There should be a severe penalty for using boost for ANYTHING.

--- zypp/parser/xml_escape_parser.cpp
+++ zypp/parser/xml_escape_parser.cpp
@@ -1,149 +1,64 @@
-/*
-IoBind Library License:
---------------------------
-
-The zlib/libpng License Copyright (c) 2003 Jonathan de Halleux
-
-This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
-
-1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
-
-2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
-
-3. This notice may not be removed or altered from any source distribution
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/parser/xml_escape_parser.cpp
+ *
 */
 
-
 #include <string>
-#include <sstream>
-#include <boost/format.hpp>
-#include <boost/spirit.hpp> 
-#include "parser_utils.hpp"
 #include "xml_escape_parser.hpp"
-
 
 namespace iobind{
 namespace parser{
-namespace detail{
 
-struct escapes : boost::spirit::symbols<std::string>
+std::string xml_escape_parser::escape(const std::string &istr) const
 {
-    escapes()
+  size_t i;
+  std::string str = istr;
+  i = str.find_first_of("<>&'\"");
+  while (i != std::string::npos)
     {
-        add
-            ("<"    , "lt")
-            (">"    , "gt")
-            ("&"    , "amp")
-            ("'"    , "apos")
-            ("\""    , "quot")
-        ;
+      switch (str[i])
+        {
+	  case '<': str.replace(i, 1, "&lt;"); i += 3; break;
+	  case '>': str.replace(i, 1, "&gt;"); i += 3; break;
+	  case '&': str.replace(i, 1, "&amp;"); i += 4; break;
+	  case '\'': str.replace(i, 1, "&apos;"); i += 5; break;
+	  case '"': str.replace(i, 1, "&quot;"); i += 5; break;
+	}
+      i = str.find_first_of("<>&'\"", i + 1);
     }
+  return str;
+}
 
-} escapes_p;
-
-struct unescapes : boost::spirit::symbols<std::string>
+std::string xml_escape_parser::unescape(const std::string &istr) const
 {
-    unescapes()
+  size_t i;
+  std::string str = istr;
+  i = str.find_first_of("&");
+  while (i != std::string::npos)
     {
-        add
-            ("lt", "<")
-            ("gt",">")
-            ("amp","&")
-            ("apos","\'")
-            ("quot","\"")
-        ;
+      if (str[i] == '&')
+        {
+	  if (!str.compare(i + 1, 3, "lt;"))
+	    str.replace(i, 4, 1, '<');
+	  else if (!str.compare(i + 1, 3, "gt;"))
+	    str.replace(i, 4, 1, '>');
+	  else if (!str.compare(i + 1, 4, "amp;"))
+	    str.replace(i, 5, 1, '&');
+	  else if (!str.compare(i + 1, 5, "apos;"))
+	    str.replace(i, 6, 1, '\'');
+	  else if (!str.compare(i + 1, 5, "quot;"))
+	    str.replace(i, 6, 1, '"');
+	}
+      i = str.find_first_of("&", i + 1);
     }
-} unescapes_p;
-
-struct unescape_from_xml_grammar : boost::spirit::grammar<unescape_from_xml_grammar>
-{
-	std::ostream& out;
-
-	explicit unescape_from_xml_grammar( std::ostream& out_)
-		:out(out_){};
-
-   template <typename ScannerT>
-   struct definition
-   {    
-        definition(unescape_from_xml_grammar const& self)  
-		{
-			using namespace boost::spirit;
-
-			begin = ch_p('&');
-			end = ch_p(';');
-			// the rest is ok
-			encoded_string=
-				*( 
-					 begin >> unescapes_p[concat_symbol(self.out)] >> end
-				   | anychar_p[concat_string(self.out)]
-				 );
-		};
-
-		boost::spirit::rule<ScannerT> encoded_string, begin, end;
-		boost::spirit::rule<ScannerT> const& start() const { return encoded_string; };
-   };
-};
-
-struct escape_to_xml_grammar : boost::spirit::grammar<escape_to_xml_grammar>
-{
-	std::ostream& out;
-
-	explicit escape_to_xml_grammar( std::ostream& out_)
-		:out(out_){};
-  
-   template <typename ScannerT>
-   struct definition
-   {    
-        definition(escape_to_xml_grammar const& self)  
-		{
-			using namespace boost::spirit;
-			concat_pre_post_symbol concatener(self.out, "&", ";");
-
-			// the rest is ok
-			encoded_string=
-				*( 
-					 escapes_p[concatener]
-				   | anychar_p[concat_string(self.out)]
-				 );
-		};
-
-		boost::spirit::rule<ScannerT> encoded_string;
-		boost::spirit::rule<ScannerT> const& start() const { return encoded_string; };
-   };
-};
-
-}; //details
-
-
-std::string xml_escape_parser::escape( std::string const& str) const
-{
-	using namespace boost::spirit;
-
-	std::ostringstream out;
-
-	parse_info<> info = boost::spirit::parse(
-               str.c_str(), 
-			   detail::escape_to_xml_grammar(out)
-			   );
-
-	return out.str();
-};
-
-std::string xml_escape_parser::unescape( std::string const& str) const
-{
-	using namespace boost::spirit;
-
-	std::ostringstream out;
-
-	parse_info<> info = boost::spirit::parse(
-               str.c_str(), 
-               detail::unescape_from_xml_grammar(out)
-			   );
-
-	return out.str();
-};
+  return str;
+}
 }; // parser
 }; // iobind
-
