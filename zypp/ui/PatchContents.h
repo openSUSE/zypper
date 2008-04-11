ff766d863cfe887c3026f5c00c35b074cf6a0e82
Michael Andres <ma@suse.de>
2008-04-11 15:17:33 +0000

    deprecate obsolete content helper classes

--- zypp/ui/PatchContents.h
+++ zypp/ui/PatchContents.h
@@ -25,8 +25,10 @@ namespace zypp
     {
 	/**
 	 * Helper class that turns Patch::atomList() into something useful.
+         *
+         * \deprecated use Patch::contets method instead.
 	 **/
-	class PatchContents
+	class ZYPP_DEPRECATED PatchContents
 	{
 	protected:
 	    typedef std::vector<ResObject::constPtr> ContentsCollection;
@@ -41,13 +43,13 @@ namespace zypp
 
 	public:
 
-	    PatchContents( Patch::constPtr patch );
+	    PatchContents( Patch::constPtr patch ) ZYPP_DEPRECATED;
 
-	    const_iterator begin() const;
-	    const_iterator end  () const;
+	    const_iterator begin() const ZYPP_DEPRECATED;
+	    const_iterator end  () const ZYPP_DEPRECATED;
 
-	    bool empty() const;
-	    size_type size() const;
+	    bool empty() const ZYPP_DEPRECATED;
+	    size_type size() const ZYPP_DEPRECATED;
 
 	private:
 	    RW_pointer<Impl> _pimpl;
--- zypp/ui/PatternContents.h
+++ zypp/ui/PatternContents.h
@@ -30,18 +30,20 @@ namespace zypp
      *
      * Joins the install_packages sets of this and all included or extending
      * pattens.
+     *
+     * \deprecated use Pattern::contents method instead.
     */
-    class PatternContents
+    class ZYPP_DEPRECATED PatternContents
     {
     public:
       class Impl;
 
     public:
       /** Ctor taking the Pattern to expand. */
-      PatternContents( const Pattern::constPtr & pattern );
+      PatternContents( const Pattern::constPtr & pattern ) ZYPP_DEPRECATED;
 
       /** Return the expanded Patterns install_packages. */
-      std::set<std::string> install_packages() const;
+      std::set<std::string> install_packages() const ZYPP_DEPRECATED;
 
     private:
       RW_pointer<Impl> _pimpl;
