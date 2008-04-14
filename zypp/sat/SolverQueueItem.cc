6b33ea14675fad4691449a8536a8339c4c47b97f
Stefan Schubert <schubi@suse.de>
2008-04-14 09:57:01 +0000

    Add new base class for solver queue

---
+++ zypp/sat/SolverQueueItem.cc
@@ -0,0 +1,99 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* SolverQueueItem.cc
+ *
+ * Copyright (C) 2008 SUSE Linux Products GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include "zypp/base/Logger.h"
+#include "zypp/sat/SolverQueueItem.h"
+
+
+/////////////////////////////////////////////////////////////////////////
+namespace zypp 
+{ ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  namespace solver
+  { /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+    namespace detail
+    { ///////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+IMPL_PTR_TYPE(SolverQueueItem);
+
+//---------------------------------------------------------------------------
+
+std::ostream &
+SolverQueueItem::dumpOn( std::ostream & os ) const
+{
+    switch (_type) {
+      case QUEUE_ITEM_TYPE_UNKNOWN       :	os << "unknown"; break;
+      case QUEUE_ITEM_TYPE_UPDATE        :	os << "update"; break;	  
+      case QUEUE_ITEM_TYPE_REQUIRE       :	os << "require"; break;
+      case QUEUE_ITEM_TYPE_CONFLICT      :	os << "conflict"; break;
+      case QUEUE_ITEM_TYPE_INSTALL_ONE_OF:	os << "install one of"; break;	  
+      default: os << "?solverqueueitem?"; break;
+    }
+    return os;
+}
+
+
+ostream&
+operator<<( ostream & os, const SolverQueueItemList & itemlist )
+{
+    for (SolverQueueItemList::const_iterator iter = itemlist.begin(); iter != itemlist.end(); ++iter) {
+	if (iter != itemlist.begin())
+	    os << "," << endl << "\t";
+	os << **iter;
+    }
+    return os;
+}
+
+//---------------------------------------------------------------------------
+
+SolverQueueItem::SolverQueueItem (SolverQueueItemType type, const ResPool & pool)
+    : _type (type)
+    , _pool (pool)
+{
+}
+
+
+SolverQueueItem::~SolverQueueItem()
+{
+}
+
+//---------------------------------------------------------------------------
+
+void
+SolverQueueItem::copy (const SolverQueueItem *from)
+{
+}
+
+
+//---------------------------------------------------------------------------
+
+
+///////////////////////////////////////////////////////////////////
+    };// namespace detail
+    /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+  };// namespace solver
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+};// namespace zypp
+/////////////////////////////////////////////////////////////////////////
---
+++ zypp/sat/SolverQueueItem.h
@@ -0,0 +1,117 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* QueueItem.h
+ *
+ * Copyright (C) 2008 SUSE Linux Products GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef ZYPP_SOLVER_DETAIL_QUEUEITEM_H
+#define ZYPP_SOLVER_DETAIL_QUEUEITEM_H
+
+#include <iosfwd>
+#include <list>
+#include <string>
+
+#include "zypp/base/ReferenceCounted.h"
+#include "zypp/base/NonCopyable.h"
+#include "zypp/base/PtrTypes.h"
+
+#include "zypp/ResPool.h"
+
+
+/////////////////////////////////////////////////////////////////////////
+namespace zypp
+{ ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  namespace solver
+  { /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+    namespace detail
+    { ///////////////////////////////////////////////////////////////////
+
+typedef enum {
+    QUEUE_ITEM_TYPE_UNKNOWN = 0,
+    QUEUE_ITEM_TYPE_UPDATE,    
+    QUEUE_ITEM_TYPE_REQUIRE,
+    QUEUE_ITEM_TYPE_CONFLICT,
+    QUEUE_ITEM_TYPE_INSTALL_ONE_OF,    
+} SolverQueueItemType;
+
+DEFINE_PTR_TYPE(SolverQueueItem);
+	
+typedef std::list<SolverQueueItem_Ptr> SolverQueueItemList;
+
+#define CMP(a,b) (((a) < (b)) - ((b) < (a)))
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : SolverQueueItem
+
+class SolverQueueItem : public base::ReferenceCounted, private base::NonCopyable {
+    
+  private:
+
+    SolverQueueItemType _type;
+    ResPool _pool;    
+
+  protected:
+
+    SolverQueueItem (SolverQueueItemType type, const ResPool & pool);
+
+  public:
+
+    virtual ~SolverQueueItem();
+
+    // ---------------------------------- I/O
+
+    virtual std::ostream & dumpOn( std::ostream & str ) const;
+
+    friend std::ostream& operator<<(std::ostream & str, const SolverQueueItem & obj)
+    { return obj.dumpOn (str); }
+    friend std::ostream& operator<<(std::ostream & str, const SolverQueueItemList & itemlist);
+
+    // ---------------------------------- accessors
+
+    ResPool pool (void) const { return _pool; }
+
+    // ---------------------------------- methods
+
+    void copy (const SolverQueueItem *from);
+
+    bool isConflict (void) const { return _type == QUEUE_ITEM_TYPE_CONFLICT; }
+    bool isRequire (void) const { return _type == QUEUE_ITEM_TYPE_REQUIRE; }
+    bool isUpdate (void) const { return _type == QUEUE_ITEM_TYPE_UPDATE; }
+    bool isInstallOneOf (void) const { return _type == QUEUE_ITEM_TYPE_INSTALL_ONE_OF; }    
+
+
+    virtual SolverQueueItem_Ptr copy (void) const = 0;
+    virtual int cmp (SolverQueueItem_constPtr item) const = 0;
+    int compare (SolverQueueItem_constPtr item) const { return CMP(_type, item->_type); }
+
+
+};
+
+///////////////////////////////////////////////////////////////////
+    };// namespace detail
+    /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+  };// namespace solver
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+};// namespace zypp
+/////////////////////////////////////////////////////////////////////////
+
+#endif // ZYPP_SOLVER_DETAIL_QUEUEITEM_H
