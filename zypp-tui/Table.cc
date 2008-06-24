d396492644df9aec125def7971e8c379de0ff577
Jan Kupec <jkupec@suse.cz>
2008-06-24 19:21:31 +0000

    - source tree reorganized, several file renames
    -----
    
    #!/bin/bash
    
    RENAMES_LIST=../renames-list
    rm $RENAMES_LIST
    svn mv zypper.h Zypper.h
    svn mv zypper.cc Zypper.cc
    svn mv zypper-command.h Command.h
    svn mv zypper-command.cc Command.cc
    svn mv zypper-tabulator.h Table.h
    svn mv zypper-tabulator.cc Table.cc
    echo "zypper.h;Zypper.h" >> $RENAMES_LIST
    echo "zypper.cc;Zypper.cc" >> $RENAMES_LIST
    echo "zypper-command.h;Command.h" >> $RENAMES_LIST
    echo "zypper-command.cc;Command.cc" >> $RENAMES_LIST
    echo "zypper-tabulator.h;Table.h" >> $RENAMES_LIST
    echo "zypper-tabulator.cc;Table.cc" >> $RENAMES_LIST
    
    # move callback headers to separate folder and remove the -callbacks prefix
    mkdir callbacks
    svn add callbacks
    for file in $(ls *-callbacks.h); do
      newfile=${file%-callbacks\.h};
      newfile="callbacks/${newfile:7}.h"
      echo "$file;$newfile" >> $RENAMES_LIST
      svn mv $file $newfile;
    done
    
    # remove 'zypper-' prefix from the sources
    for file in $(ls zypper-*); do
      echo "$file;${file:7}" >> $RENAMES_LIST
      svn mv $file ${file:7};
    done
    
    # adapt include paths
    sed -i -e 's/zypper\.h/Zypper.h/g' $(grep -Rl 'zypper\.h' *)
    sed -i -e 's/zypper\.cc/Zypper.cc/g' $(grep -Rl 'zypper\.cc' *)
    sed -i -e 's/zypper-command\.\(h\|cc\)/Command.\1/g' $(grep -Rl 'zypper-command\.\(h\|cc\)' *)
    sed -i -e 's/zypper-tabulator\.\(h\|cc\)/Table.\1/g' $(grep -Rl 'zypper-tabulator\.\(h\|cc\)' *)
    sed -i -e 's/zypper-\(\w*\)-callbacks\.h/callbacks\/\1.h/g' $(grep -Rl 'zypper-\w*-callbacks\.h' *)
    sed -i -e 's/zypper-\(.*\.\(h\|cc\)\)/\1/g' $(grep -Rl 'zypper-.*\.\(h\|cc\)' *)
    for file in $(find -name '*.h'); do
      file1=${file:2}
      regex=${file1/\.h/\\\.h}
      regex=${regex/\//\\\/};
      sed -i -e "s/\"$regex\"/\"..\\/$regex\"/g" callbacks/*
    done
    
    -----
    
    zypper.h;Zypper.h
    zypper.cc;Zypper.cc
    zypper-command.h;Command.h
    zypper-command.cc;Command.cc
    zypper-tabulator.h;Table.h
    zypper-tabulator.cc;Table.cc
    zypper-keyring-callbacks.h;callbacks/keyring.h
    zypper-locks-callbacks.h;callbacks/locks.h
    zypper-media-callbacks.h;callbacks/media.h
    zypper-repo-callbacks.h;callbacks/repo.h
    zypper-rpm-callbacks.h;callbacks/rpm.h
    zypper-getopt.cc;getopt.cc
    zypper-getopt.h;getopt.h
    zypper-info.cc;info.cc
    zypper-info.h;info.h
    zypper-locks.cc;locks.cc
    zypper-locks.h;locks.h
    zypper-main.cc;main.cc
    zypper-main.h;main.h
    zypper-misc.cc;misc.cc
    zypper-misc.h;misc.h
    zypper-prompt.cc;prompt.cc
    zypper-prompt.h;prompt.h
    zypper-repos.cc;repos.cc
    zypper-repos.h;repos.h
    zypper-richtext.cc;richtext.cc
    zypper-richtext.h;richtext.h
    zypper-search.cc;search.cc
    zypper-search.h;search.h
    zypper-utils.cc;utils.cc
    zypper-utils.h;utils.h

---
+++ zypp-tui/Table.cc
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <readline/readline.h>
+
+#include "zypp/base/Logger.h"
+
+#include "Table.h"
+using namespace std;
+#include <wchar.h>
+
+TableStyle Table::defaultStyle = Ascii;
+
+static
+const char * lines[][3] = {
+  { "|", "-", "+"},		///< Ascii
+  // utf 8. TODO wchars?
+  { "\xE2\x94\x82", "\xE2\x94\x80", "\xE2\x94\xBC"}, ///< light
+  { "\xE2\x94\x83", "\xE2\x94\x81", "\xE2\x95\x8B"}, ///< heavy
+  { "\xE2\x95\x91", "\xE2\x95\x90", "\xE2\x95\xAC"}, ///< double
+  { "\xE2\x94\x86", "\xE2\x94\x84", "\xE2\x94\xBC"}, ///< light 3
+  { "\xE2\x94\x87", "\xE2\x94\x85", "\xE2\x94\x8B"}, ///< heavy 3
+  { "\xE2\x94\x82", "\xE2\x94\x81", "\xE2\x94\xBF"}, ///< v light, h heavy
+  { "\xE2\x94\x82", "\xE2\x95\x90", "\xE2\x95\xAA"}, ///< v light, h double
+  { "\xE2\x94\x83", "\xE2\x94\x80", "\xE2\x95\x82"}, ///< v heavy, h light
+  { "\xE2\x95\x91", "\xE2\x94\x80", "\xE2\x95\xAB"}, ///< v double, h light
+};
+
+// A non-ASCII string has 3 different lengths:
+// - bytes
+// - characters (non-ASCII ones have multiple bytes in UTF-8)
+// - columns (Chinese characters are 2 columns wide)
+// In #328918 see how confusing these leads to misalignment.
+
+// return the number of columns in str, or -1 if there's an error
+static
+int string_to_columns_e (const string& str) {
+  // from smpppd.src.rpm/format.cc, thanks arvin
+
+  const char* ptr = str.c_str ();
+  size_t s_bytes = str.length ();
+  int s_cols = 0;
+
+  mbstate_t shift_state;
+  memset (&shift_state, 0, sizeof (shift_state));
+
+  wchar_t wc;
+  size_t c_bytes;
+
+  // mbrtowc produces one wide character from a multibyte string
+  while ((c_bytes = mbrtowc (&wc, ptr, s_bytes, &shift_state)) > 0) {
+    if (c_bytes >= (size_t) -2) // incomplete (-2) or invalid (-1) sequence
+      return -1;
+
+    s_cols += wcwidth (wc);
+
+    s_bytes -= c_bytes;
+    ptr += c_bytes;
+  }
+
+  return s_cols;
+}
+
+static
+unsigned string_to_columns (const string& str) {
+  int c = string_to_columns_e (str);
+  if (c < 0)
+    return str.length();	// fallback if there was an error
+  else
+    return (unsigned) c;
+}
+
+void TableRow::add (const string& s) {
+  _columns.push_back (s);
+}
+
+unsigned int TableRow::cols( void ) const {
+  return _columns.size();
+}
+
+// 1st implementation: no width calculation, just tabs
+void TableRow::dumbDumpTo (ostream &stream) const {
+  bool seen_first = false;
+  for (container::const_iterator i = _columns.begin (); i != _columns.end (); ++i) {
+    if (seen_first)
+      stream << '\t';
+    seen_first = true;
+
+    stream << *i;
+  }
+  stream << endl;
+}
+
+void TableRow::dumpTo (ostream &stream, const vector<unsigned>& widths,
+		       TableStyle st) const {
+  const char * vline = lines[st][0];
+
+  unsigned int ssize = 0; // string size in columns
+  bool seen_first = false;
+  container::const_iterator
+    i = _columns.begin (),
+    e = _columns.end ();
+
+  stream.setf (ios::left, ios::adjustfield);
+  for (unsigned c = 0; i != e ; ++i, ++c) {
+    if (seen_first) {
+      stream.width (0);
+      // pad vertical line with spaces
+      stream << ' ' << vline << ' ';
+    }
+    seen_first = true;
+
+    // stream.width (widths[c]); // that does not work with multibyte chars
+    const string & s = *i;
+    ssize = string_to_columns (s);
+    if (ssize > widths[c])
+      stream << (s.substr(0, widths[c] - 2) + "->"); //! \todo FIXME cut at the correct place
+    else
+    {
+      stream << s;
+      stream.width (widths[c] - ssize);
+    } 
+    stream << "";
+  }
+  stream << endl;
+}
+
+// ----------------------( Table )---------------------------------------------
+
+Table::Table() :
+  _has_header (false),
+  _max_col (0),
+  _width(0),
+  _style (defaultStyle)
+{
+  //! \todo move this to utils
+
+  const char *cols_env = getenv("COLUMNS");
+  if (cols_env)
+    _screen_width  = ::atoi (cols_env);
+  else
+  {
+    ::rl_initialize();
+    //::rl_reset_screen_size();
+    ::rl_get_screen_size (NULL, &_screen_width);
+    DBG << "readline says we have " << _screen_width << " char wide console screen" << endl;
+  }
+
+  // safe default
+  if (!_screen_width)
+     _screen_width = 80;
+
+  DBG << "got screen width of " << _screen_width << endl;
+}
+
+void Table::add (const TableRow& tr) {
+  _rows.push_back (tr);
+  updateColWidths (tr);
+}
+
+void Table::setHeader (const TableHeader& tr) {
+  _has_header = true;
+  _header = tr;
+  updateColWidths (tr);
+}
+
+void Table::allowAbbrev(unsigned column) {
+  if (column >= _abbrev_col.size()) {
+    _abbrev_col.reserve(column + 1);
+    _abbrev_col.insert(_abbrev_col.end(), column - _abbrev_col.size() + 1, false);
+  }
+  _abbrev_col[column] = true;
+}
+
+void Table::updateColWidths (const TableRow& tr) {
+  _width = -3;
+
+  TableRow::container::const_iterator
+    i = tr._columns.begin (),
+    e = tr._columns.end ();
+  for (unsigned c = 0; i != e; ++i, ++c) {
+    // ensure that _max_width[c] exists
+    if (_max_col < c)
+      _max_col = c;
+    _max_width.resize (_max_col + 1);
+
+    unsigned &max = _max_width[c];
+    unsigned cur = string_to_columns (*i);
+
+    if (max < cur)
+      max = cur;
+
+    _width += max + 3;
+  }
+}
+
+void Table::dumpRule (ostream &stream) const {
+  const char * hline = lines[_style][1];
+  const char * cross = lines[_style][2];
+
+  bool seen_first = false;
+  
+  stream.width (0);
+  for (unsigned c = 0; c <= _max_col; ++c) {
+    if (seen_first) {
+      stream << hline << cross << hline;
+    }
+    seen_first = true;
+    // FIXME: could use fill character if hline were a (wide) character
+    for (unsigned i = 0; i < _max_width[c]; ++i) {
+      stream << hline;
+    }
+  }
+  stream << endl;
+}
+
+void Table::dumpTo (ostream &stream) const {
+
+  // reset column widths for columns that can be abbreviated
+  //! \todo allow abbrev of multiple columns?
+  unsigned c = 0;
+  for (vector<bool>::const_iterator it = _abbrev_col.begin();
+      it != _abbrev_col.end() && c <= _max_col; ++it, ++c) {
+    if (*it && _width > _screen_width) {
+      _max_width[c] -= _width - _screen_width;
+      break;
+    }
+  }
+
+  if (_has_header) {
+    _header.dumpTo (stream, _max_width, _style);
+    dumpRule (stream);
+  }
+
+  container::const_iterator
+    b = _rows.begin (),
+    e = _rows.end (),
+    i;
+  for (i = b; i != e; ++i) {
+    i->dumpTo (stream, _max_width, _style);
+  }
+}
+
+void Table::style (TableStyle st) {
+  if (st < _End)
+    _style = st;
+}
+
+void Table::sort (unsigned by_column) {
+  if (by_column > _max_col) {
+    ERR << "by_column >= _max_col (" << by_column << ">=" << _max_col << ")" << endl;
+    return;
+  }
+
+  TableRow::Less comp (by_column);
+  _rows.sort (comp);
+}
+
+// Local Variables:
+// c-basic-offset: 2
+// End:
---
+++ zypp-tui/Table.h
@@ -0,0 +1,141 @@
+/*-----------------------------------------------------------*- c++ -*-\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+
+#ifndef ZYPPER_TABULATOR_H
+#define ZYPPER_TABULATOR_H
+
+#include <string>
+#include <iosfwd>
+#include <list>
+#include <vector>
+using std::string;
+using std::ostream;
+using std::list;
+using std::vector;
+
+//! table drawing style
+enum TableStyle {
+  Ascii,			///< | - +
+  Light,
+  Heavy,
+  Double,
+  Light3,
+  Heavy3,
+  LightHeavy,
+  LightDouble,
+  HeavyLight,
+  DoubleLight,
+  _End,				///< sentinel
+};
+
+class TableRow {
+public:
+  //! Constructor. Reserve place for c columns.
+  TableRow (unsigned c = 0) {
+    _columns.reserve (c);
+  }
+
+  void add (const string& s);
+
+  // return number of columns
+  unsigned int cols( void ) const;
+
+  //! tab separated output
+  void dumbDumpTo (ostream &stream) const;
+  //! output with field widths
+  void dumpTo (ostream &stream, const vector<unsigned>& widths,
+	       TableStyle st) const;
+
+  typedef vector<string> container;
+
+  // BinaryPredicate
+  struct Less {
+    unsigned _by_column;
+    Less (unsigned by_column): _by_column (by_column) {}
+
+    bool operator ()(const TableRow& a, const TableRow& b) const {
+      return a._columns[_by_column] < b._columns[_by_column];
+    }
+  };
+
+private:
+  container _columns;
+  friend class Table;
+};
+
+class TableHeader : public TableRow {
+public:
+  //! Constructor. Reserve place for c columns.
+  TableHeader (unsigned c = 0): TableRow (c) {}
+};
+
+inline
+TableRow& operator << (TableRow& tr, const string& s) {
+  tr.add (s);
+  return tr;
+}
+
+class Table {
+public:
+  static TableStyle defaultStyle;
+
+  void add (const TableRow& tr);
+  void setHeader (const TableHeader& tr);
+  void dumpTo (ostream& stream) const;
+  bool empty () const { return _rows.empty(); }
+  typedef list<TableRow> container;
+
+  void style (TableStyle st);
+  void sort (unsigned by_column);	// columns start with 0...
+  void allowAbbrev(unsigned column);
+
+  Table ();
+
+private:
+  void dumpRule (ostream &stream) const;
+  void updateColWidths (const TableRow& tr);
+
+  bool _has_header;
+  TableHeader _header;
+  container _rows;
+  //! maximum column index seen in this table
+  unsigned _max_col;
+  //! maximum width of respective columns
+  mutable vector<unsigned> _max_width;
+  //! table width (columns)
+  int _width;
+  //! table drawing style
+  TableStyle _style;
+  //! console screen width as retrieved by readline
+  int _screen_width;
+  //! whether to abbreviate the column if needed
+  vector<bool> _abbrev_col;
+};
+
+inline
+Table& operator << (Table& table, const TableRow& tr) {
+  table.add (tr);
+  return table;
+}
+
+inline
+Table& operator << (Table& table, const TableHeader& tr) {
+  table.setHeader (tr);
+  return table;
+}
+
+inline
+ostream& operator << (ostream& stream, const Table& table) {
+  table.dumpTo (stream);
+  return stream;
+}
+// Local Variables:
+// c-basic-offset: 2
+// End:
+#endif
