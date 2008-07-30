bc2caece4b892054566a11c5a3dd6858cb8d6891
Ladislav Slezak <lslezak@suse.cz>
2008-07-30 09:51:21 +0000

    - use newline separator in ZYppFactoryException message so it can be   displayed in a popup window in GUI

--- zypp/ZYppFactory.cc
+++ zypp/ZYppFactory.cc
@@ -358,7 +358,7 @@ namespace zypp
       /*--------------------------------------------------*/
       if ( globalLock.zyppLocked() )
       {
-	std::string t = str::form(N_("System management is locked by the application with pid %d (%s). "
+	std::string t = str::form(N_("System management is locked by the application with pid %d (%s).\n"
                                   "Please close this application before trying again."),
                                   globalLock.locker_pid(),
                                   globalLock.locker_name().c_str()
