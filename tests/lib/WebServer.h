03d2e5cc3baaad4250af7212c3866a87f1987eda
Duncan Mac-Vicar P <dmacvicar@suse.de>
2008-10-17 14:16:30 +0000

    add docs

--- tests/lib/WebServer.h
+++ tests/lib/WebServer.h
@@ -10,16 +10,46 @@
 
 struct shttpd_ctx;
 
+/**
+ *
+ * Starts a webserver to simulate remote transfers in
+ * testcases
+ *
+ * \code
+ * #include "WebServer.h"
+ *
+ * BOOST_AUTO_TEST_CASE(Foo)
+ * {
+ *
+ *      WebServer web((Pathname(TESTS_SRC_DIR) + "/datadir").c_str() );
+ *      web.start();
+ *
+ *     MediaSetAccess media( Url("http://localhost:9099"), "/" );
+ *     
+ *     // do something with the url
+ * 
+ *
+ *     web.stop();
+ *
+ * \endcode
+ */
 class WebServer
 {
  public:
+  /**
+   * creates a web server on \ref root and \port
+   */
   WebServer(const zypp::Pathname root, unsigned int port=9099);
   ~WebServer();
+  /**
+   * Starts the webserver worker thread
+   */
   void start();
-
+  /**
+   * Stops the worker thread
+   */
   void stop();
  
-
  private:
 
   void setStrOption( std::string name, std::string value);
