0e910ca254e4c9daf1830812380c4540bb21194b
Ján Kupec <jkupec@suse.cz>
2009-01-26 10:33:06 +0100

    backup

--- devel/devel.jkupec/play.cc
+++ devel/devel.jkupec/play.cc
@@ -2,9 +2,24 @@
 
 #include "zypp/base/LogTools.h"
 
+#include "zypp/parser/HistoryLogReader.h"
+#include "zypp/parser/ParseException.h"
+
 using std::endl;
 using std::cout;
+using namespace zypp;
 
+struct HistoryItemCollector
+{
+//  vector<HistoryItem::Ptr> items;
+
+  bool operator()( const HistoryItem::Ptr & item_ptr )
+  {
+//    items.insert(item);
+    cout << *item_ptr << endl;
+    return true;
+  }
+};
 
 // ---------------------------------------------------------------------------
 
@@ -12,7 +27,19 @@ int main( int argc, const char * argv[] )
 {
   --argc; ++argv; // skip arg 0
 
+  HistoryItemCollector ic;
+  parser::HistoryLogReader reader(*argv, ic);
+
+  try
+  {
+    reader.readAll();
+  }
+  catch (const parser::ParseException & e)
+  {
+    cout << "error in " << *argv << ":" << endl;
+    cout << e.asUserHistory() << endl;
+  }
+
   cout << "done" << endl;
   return 0;
 }
-
