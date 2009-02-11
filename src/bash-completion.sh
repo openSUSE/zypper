2b8a5de712d9971a9169f98d301bc038c7d886c6
Josef Reidinger <jreidinger@suse.cz>
2009-02-11 13:51:54 +0100

    Bash completion: allow repo alias completion also for modifyrepo and removerepo.

--- src/bash-completion.sh
+++ src/bash-completion.sh
@@ -55,6 +55,7 @@ _zypper() {
 		;;
 		removerepo)
       opts=$(LC_ALL=C $ZYPPER help $prev 2>&1 | sed -e "1,/$magic_string/d" -e 's/.*--/--/' -e 's/ .*//')
+      opts="${opts} $(LC_ALL=C $ZYPPER  lr | sed -e '1,2 d' -e 's/^[0-9]\+[[:space:]]\+|[[:space:]]*\([^|]\+\)|.*$/\1/' -e 's/[[:space:]]*$//')"
       COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
 		;;
 		renamerepo)
@@ -64,6 +65,7 @@ _zypper() {
 		;;
 		modifyrepo)
       opts=$(LC_ALL=C $ZYPPER help $prev 2>&1 | sed -e "1,/$magic_string/d" -e 's/.*--/--/' -e 's/ .*//')
+      opts="${opts} $(LC_ALL=C $ZYPPER  lr | sed -e '1,2 d' -e 's/^[0-9]\+[[:space:]]\+|[[:space:]]*\([^|]\+\)|.*$/\1/' -e 's/[[:space:]]*$//')"
       COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
 		;;
 		refresh)
