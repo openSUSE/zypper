14b5dec85100cca47d17e14515a19652bdc3e047
Ján Kupec <jkupec@suse.cz>
2009-03-05 14:47:12 +0100

    Fixed FindReadline to find shared lib, not static.

--- cmake/modules/FindReadline.cmake
+++ cmake/modules/FindReadline.cmake
@@ -18,13 +18,18 @@ FIND_PATH(READLINE_INCLUDE_DIR readline/readline.h
   /usr/include/readline
 )
 
-FIND_LIBRARY(READLINE_LIBRARY NAMES readline
+# make find_library look only for shared lib
+# otherwise it would find the static libreadline.a 
+SET(CMAKE_FIND_LIBRARY_SUFFIXES_BACKUP ${CMAKE_FIND_LIBRARY_SUFFIXES})
+SET(CMAKE_FIND_LIBRARY_SUFFIXES ".so")
+FIND_LIBRARY(READLINE_LIBRARY readline
   PATHS
   /usr/lib
   /usr/lib64
   /usr/local/lib
   /usr/local/lib64
 )
+SET(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES_BACKUP})
 
 if(READLINE_INCLUDE_DIR AND READLINE_LIBRARY)
    MESSAGE( STATUS "readline found: includes in ${READLINE_INCLUDE_DIR}, library in ${READLINE_LIBRARY}")
