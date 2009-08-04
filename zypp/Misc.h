144325f0cf1f677d7cef5207f3bd2b394989e072
Michael Andres <ma@suse.de>
2009-08-04 16:31:55 +0200

    Add compat Misc.h

---
+++ zypp/Misc.h
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	zypp/Misc.h
+ *
+*/
+#ifndef ZYPP_MISC_H
+#define ZYPP_MISC_H
+
+#include "zypp/misc/DefaultLoadSystem.h"
+#include "zypp/misc/CheckAccessDeleted.h"
+
+#endif // ZYPP_MISC_H
