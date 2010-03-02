ab0f62214cceb42fe885a32d5f6b375c87ccbafc
Ján Kupec <jkupec@suse.cz>
2010-03-02 08:04:39 +0100

    Bump version for 11.3 development

--- VERSION.cmake
+++ VERSION.cmake
@@ -30,8 +30,8 @@
 #   note in the changes file.
 #
 SET(VERSION_MAJOR "1")
-SET(VERSION_MINOR "2")
-SET(VERSION_PATCH "21")
+SET(VERSION_MINOR "4")
+SET(VERSION_PATCH "0")
 
 # LAST RELEASED: 1.2.21
 #=======
