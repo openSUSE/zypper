f427000ce37f010f00a753999d07d217f4173282
Michael Andres <ma@suse.de>
2010-09-29 13:04:51 +0200

    Disable SolverRequester testcases - NEED REVIEW

---
+++ tests/SolverRequester_DISABLED_test.cc
@@ -0,0 +1,10 @@
+#include "TestSetup.h"
+BOOST_AUTO_TEST_CASE(disabled)
+{
+#warning ***
+#warning *** TESTCASES DISABLED - NEED REVIEW ***
+#warning ***
+  std::cout << "***" << endl;
+  std::cout << "*** TESTCASES DISABLED - NEED REVIEW ***" << endl;
+  std::cout << "***" << endl;
+}
