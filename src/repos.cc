25135c231d58ea4e35c757251b9fdbf3fa11093c
Ján Kupec <jkupec@suse.cz>
2010-10-15 17:52:50 +0200

    Fixes for "Auto-refresh services before loading repos."

--- src/repos.cc
+++ src/repos.cc
@@ -489,27 +489,25 @@ void report_unknown_repos(Out & out, list<string> not_found)
 template <class Container>
 void do_init_repos(Zypper & zypper, const Container & container)
 {
-  RepoManager & manager = zypper.repoManager();
-  RuntimeData & gData = zypper.runtimeData();
-
   MIL << "Refreshing autorefresh services." << endl;
 
-  const list<ServiceInfo> & services = manager.knownServices();
+  const list<ServiceInfo> & services = zypper.repoManager().knownServices();
+  bool called_refresh = false;
   for_(s, services.begin(), services.end())
   {
-    bool called_refresh = false;
     if (s->enabled() && s->autorefresh())
     {
       refresh_service(zypper, *s);
       called_refresh = true;
     }
-
-    // reinitialize the repo manager to re-read the list of repos
-    if (called_refresh)
-      zypper.initRepoManager();
   }
+  // reinitialize the repo manager to re-read the list of repos
+  if (called_refresh)
+    zypper.initRepoManager();
 
   MIL << "Going to initialize repositories." << endl;
+  RepoManager & manager = zypper.repoManager();
+  RuntimeData & gData = zypper.runtimeData();
 
   // load gpg keys
   init_target(zypper);
