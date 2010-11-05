03eabaeeb217b2f1ec801ecc75f57b16fc64ae8f
Duncan Mac-Vicar P <dmacvicar@suse.de>
2010-11-05 14:53:53 +0100

    remove noise from test, as I want to use it in the documentation as example

--- tests/zypp/Date_test.cc
+++ tests/zypp/Date_test.cc
@@ -1,25 +1,10 @@
-//
-// tests for zypp/Date.h
-//
-
-#include <iostream>
-#include <string>
-
-#include "zypp/base/Logger.h"
 #include "zypp/Date.h"
 
-// Boost.Test
 #include <boost/test/auto_unit_test.hpp>
-
-using boost::unit_test::test_suite;
-using boost::unit_test::test_case;
-
-using namespace std;
-using namespace zypp;
 
 BOOST_AUTO_TEST_CASE(date_test)
 {
-  string format = "%Y-%m-%d %H:%M:%S";
-  string date = "2009-02-14 00:31:30";
-  BOOST_CHECK_EQUAL(Date(date,format).form(format), date);
+  std::string format = "%Y-%m-%d %H:%M:%S";
+  std::string date = "2009-02-14 00:31:30";
+  BOOST_CHECK_EQUAL(zypp::Date(date,format).form(format), date);
 }
