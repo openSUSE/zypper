18adf8c4ad0fbd5f5c2c5c4e3948432a9adea623
Duncan Mac-Vicar P <dmacvicar@suse.de>
2010-12-21 17:33:02 +0100

    build require libproxy

--- libzypp.spec.cmake
+++ libzypp.spec.cmake
@@ -34,6 +34,7 @@ BuildRequires:  gcc-c++
 BuildRequires:  gettext-devel
 BuildRequires:  graphviz
 BuildRequires:  libxml2-devel
+BuildRequires:  libproxy-devel >= 0.4.6
 
 BuildRequires:  libsatsolver-devel >= 0.14.17
 %if 0%{?suse_version}
