a5cb48f885fcd77644bb4fb85edf7103d68605ee
Michael Andres <ma@suse.de>
2011-05-23 16:07:00 +0200

    Disable rpm V3toV4 conversion while not working with rpm 4.9

--- zypp/target/rpm/librpmDb.cv3.cc
+++ zypp/target/rpm/librpmDb.cv3.cc
@@ -8,7 +8,8 @@
 \---------------------------------------------------------------------*/
 /** \file zypp/target/rpm/librpmDb.cv3.cc
  *
-*/
+ */
+#if 0
 #include "librpm.h"
 extern "C"
 {
@@ -697,7 +698,27 @@ void internal_convertV3toV4( const Pathname & v3db_r, const librpmDb::constPtr &
     MIL << endl;
   }
 }
+#endif
 
+#include <iostream>
+
+#include "zypp/base/Logger.h"
+
+#include "zypp/target/rpm/librpmDb.h"
+#include "zypp/target/rpm/RpmCallbacks.h"
+#include "zypp/ZYppCallbacks.h"
+
+using namespace std;
+
+#undef Y2LOG
+#define Y2LOG "librpmDb"
+
+namespace zypp
+{
+namespace target
+{
+namespace rpm
+{
 /******************************************************************
 *
 *
@@ -713,7 +734,10 @@ void convertV3toV4( const Pathname & v3db_r, const librpmDb::constPtr & v4db_r )
   report->start(v3db_r);
   try
   {
-    internal_convertV3toV4( v3db_r, v4db_r, report );
+    // Does no longer work with rpm 4.9.
+    // internal_convertV3toV4( v3db_r, v4db_r, report );
+    INT << "Unsupported: Convert rpm3 database to rpm4" << endl;
+    ZYPP_THROW(RpmDbOpenException(Pathname("/"), v3db_r));
   }
   catch (RpmException & excpt_r)
   {
