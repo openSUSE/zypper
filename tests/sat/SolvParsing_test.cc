8da471299d52ef6b2be95bfd6c8541e4db09ef85
Michael Andres <ma@suse.de>
2012-03-29 11:50:08 +0200

    fix testcase

--- tests/sat/SolvParsing_test.cc
+++ tests/sat/SolvParsing_test.cc
@@ -107,9 +107,9 @@ BOOST_AUTO_TEST_CASE(attributes)
             //solvable:url: http://www.openssl.org/
             //solvable:buildtime: 1165493634
             //solvable:installsize: 3845
-            BOOST_CHECK_EQUAL(p->installSize(), ByteCount(3845, ByteCount::K));
+            BOOST_CHECK_EQUAL(p->installSize(), ByteCount(3937193));
             //solvable:downloadsize: 909
-            BOOST_CHECK_EQUAL(p->downloadSize(), ByteCount(909, ByteCount::K));
+            BOOST_CHECK_EQUAL(p->downloadSize(), ByteCount(930588));
             //solvable:mediadir: rpm/i586
             //solvable:mediafile: openssl-devel-0.9.8d-17.2.i586.rpm
             //solvable:license: BSD License and BSD-like, Other License(s), see package
