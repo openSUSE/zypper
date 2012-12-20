125e493511bdb6a1970b118f8adeb04cafc05a7e
Michael Andres <ma@suse.de>
2012-12-20 09:04:55 +0100

    Fix install counter (bnc#793285)

--- src/solve-commit.cc
+++ src/solve-commit.cc
@@ -748,7 +748,7 @@ void solve_and_commit (Zypper & zypper)
           gData.show_media_progress_hack = true;
           // Total packages to download & install.
           // To be used to write overall progress of retrieving packages.
-          gData.commit_pkgs_total = summary.packagesToGetAndInstall();
+          gData.commit_pkgs_total = God->resolver()->getTransaction().actionSize();
           gData.commit_pkg_current = 0;
           // To be used to show overall progress of rpm transactions.
           gData.rpm_pkgs_total = summary.packagesToGetAndInstall() + summary.packagesToRemove();
