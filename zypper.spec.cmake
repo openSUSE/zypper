ae5697bad08c58b2ea7fd6cdd24dc631dd616ab5
Michael Andres <ma@suse.de>
2013-01-18 20:09:27 +0100

    BuildRequire libzypp-9.33.0

--- zypper.spec.cmake
+++ zypper.spec.cmake
@@ -11,7 +11,7 @@
 # norootforbuild
 
 Name:           @PACKAGE@
-BuildRequires:  libzypp-devel >= 9.30.0
+BuildRequires:  libzypp-devel >= 9.33.0
 BuildRequires:  boost-devel >= 1.33.1
 BuildRequires:  gettext-devel >= 0.15
 BuildRequires:  readline-devel >= 5.1
