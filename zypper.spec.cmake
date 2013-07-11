2b946cba49256de6f02a6c58ed06cfef1175eaa7
Michael Andres <ma@suse.de>
2013-07-11 12:23:08 +0200

    format specfile

--- zypper.spec.cmake
+++ zypper.spec.cmake
@@ -1,7 +1,7 @@
 #
-# spec file for package zypper.spec.cmake
+# spec file for package zypper
 #
-# Copyright (c) 2013 SUSE LINUX Products GmbH, Nuernberg, Germany.
+# Copyright (c) 2006-2013 SUSE LINUX Products GmbH, Nuernberg, Germany.
 #
 # All modifications and additions to the file contributed by third parties
 # remain the property of their copyright owners, unless otherwise agreed
