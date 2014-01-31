e04e16d9b48c943e390db4eabf03ed9df71da524
Michael Andres <ma@suse.de>
2014-01-31 17:46:06 +0100

    Simplify printing [indented] richtext.

--- zypp-tui/utils/richtext.h
+++ zypp-tui/utils/richtext.h
@@ -1,10 +1,26 @@
 #ifndef ZYPPERRICHTEXT_H_
 #define ZYPPERRICHTEXT_H_
 
+#include <iosfwd>
+#include <zypp/base/String.h>
 
-#include <string>
+/** Convert . */
+//std::string processRichText( const std::string& text );
 
-std::string processRichText(const std::string& text);
+/** Print [Rich]Text optionally indented. */
+inline std::ostream & printRichText( std::ostream & str, std::string text, unsigned indent_r = 0U )
+{
+  std::string processRichText( const std::string& text );
+
+  if ( text.empty() )
+    return str;
+
+  if ( text.find("DT:Rich") != std::string::npos )
+    text = processRichText( text );
+
+  return str::printIndented( str, text, indent_r );	// even unindented as it also asaerts a trailing '/n'
+}
+
 
 #endif
 
