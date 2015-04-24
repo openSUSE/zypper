5ea5421f0a9783e4bc7c488c2200f45bec4b14d7
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-04-24 14:43:36 +0200

    aptitude: add os-specific package-maps

--- tools/apt-packagemap.d/10-packagemap.pm
+++ tools/apt-packagemap.d/10-packagemap.pm
@@ -8,19 +8,38 @@ libsasl2-dev		cyrus-sasl-devel
 zlib1g-dev		zlib-devel
 locate			findutils-locate
 open-iscsi-utils	open-iscsi
-openssh-server		openssh
 pep8			python-pep8
-pylint			python-pylint
 python-cherrypy3	python-CherryPy
-python-cheetah		python-Cheetah
-python-gflags		python-python-gflags
 python-libvirt		libvirt-python
 python-libxml2		libxml2-python
-python-migrate		python-sqlalchemy-migrate
 python-mysqldb		python-mysql
 python-pysqlite2	python-pysqlite
 vim-nox			vim
 );
+
+my %ospkgmap = (
+suse => {qw(
+	openssh-server		openssh
+	pylint			python-pylint
+	python-migrate		python-sqlalchemy-migrate
+	)},
+fedora => {qw(
+	libldap2-dev		openldap-devel
+	locate			findutils
+	open-iscsi-utils	iscsi-initiator-utils
+	python-cherrypy3	python-cherrypy
+	python-mysqldb		MySQL-python
+	python-pysqlite2	python-apsw
+	vim-nox			vim-minimal
+	)},
+);
+
+foreach my $os (keys(%ospkgmap)) {
+	if(os_is($os)) {
+		my $map = $ospkgmap{$os};
+		foreach my $k (keys(%$map)) { $pkgmap{$k}=$map->{$k} }
+	}
+}
 
 foreach(@ARGV) {
 	my $p=$pkgmap{$_};
