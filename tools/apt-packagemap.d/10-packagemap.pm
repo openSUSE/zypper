040f905429c2490b2f7d578ba474c281ebd31af1
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-04-28 14:38:22 +0200

    aptitude: re-order rules
    this makes it work better with mandriva+mageia
    and possibly other distributions that are closer to Fedora's naming

--- tools/apt-packagemap.d/10-packagemap.pm
+++ tools/apt-packagemap.d/10-packagemap.pm
@@ -6,31 +6,31 @@ iputils-ping		iputils
 libldap2-dev		openldap2-devel
 libsasl2-dev		cyrus-sasl-devel
 zlib1g-dev		zlib-devel
-locate			findutils-locate
+locate			findutils
 open-iscsi-utils	open-iscsi
 pep8			python-pep8
-python-cherrypy3	python-CherryPy
+python-cherrypy3	python-cherrypy
 python-libvirt		libvirt-python
 python-libxml2		libxml2-python
 python-mysqldb		python-mysql
 python-pysqlite2	python-pysqlite
-vim-nox			vim
+vim-nox			vim-minimal
 );
 
 my %ospkgmap = (
 suse => {qw(
+	locate			findutils-locate
 	openssh-server		openssh
 	pylint			python-pylint
+	python-cherrypy3	python-CherryPy
 	python-migrate		python-sqlalchemy-migrate
+	vim-nox			vim
 	)},
 "fedora|centos|rhel" => {qw(
 	libldap2-dev		openldap-devel
-	locate			findutils
 	open-iscsi-utils	iscsi-initiator-utils
-	python-cherrypy3	python-cherrypy
 	python-mysqldb		MySQL-python
 	python-pysqlite2	python-apsw
-	vim-nox			vim-minimal
 	)},
 );
 
