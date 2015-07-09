9d31f04ddb6a33f8dd83c905abf3f559fffb4d57
Michael Andres <ma@suse.de>
2015-07-09 14:54:54 +0200

    MbsWriteWrapped: multi-byte and ansi SGR aware formated writer

--- src/utils/text.cc
+++ src/utils/text.cc
@@ -5,10 +5,8 @@
                              |__/|_|  |_|
 \*---------------------------------------------------------------------------*/
 
-#include <cwchar>
 #include <cstring>
-#include <ostream>
-
+#include <boost/utility/string_ref.hpp>
 #include "utils/text.h"
 
 using namespace std;
@@ -142,100 +140,3 @@ std::string mbs_substr_by_width(
 }
 
 // ---------------------------------------------------------------------------
-
-void mbs_write_wrapped(ostream & out, const string & text,
-    unsigned indent, unsigned wrap_width, int initial)
-{
-  const char * s = text.c_str();
-  size_t s_bytes = text.length();
-  const char * prevwp = s;
-  const char * linep = s;
-  wchar_t wc;
-  size_t bytes_read;
-  bool in_word = false;
-  bool in_ctrlseq = false;
-
-  mbstate_t shift_state;
-  memset (&shift_state, 0, sizeof (shift_state));
-
-  unsigned col = 0;
-  unsigned toindent = initial < 0 ? indent : initial;
-  do
-  {
-    // indentation
-    if (!col)
-    {
-      out << string(toindent, ' ');
-      col = toindent;
-    }
-
-    bytes_read = mbrtowc (&wc, s, s_bytes, &shift_state);
-    if (bytes_read >= (size_t) -2) // incomplete (-2) or invalid (-1) sequence
-    {
-      out << endl << "WCHAR ERROR" << endl;
-      return;
-    }
-    if (bytes_read > 0)
-    {
-      col += wcwidth_without_ctrlseq(wc, in_ctrlseq);
-
-      if (::iswspace(wc))
-        in_word = false;
-      //else if (::wcscmp(wc, L"\n"))
-      //{
-      //  if (!in_word)
-      //    prevwp = s;
-      //  in_word = true;
-      //}
-      else
-      {
-        if (!in_word)
-          prevwp = s;
-        in_word = true;
-      }
-
-      // current wc exceeded the wrap width
-      if (col > wrap_width)
-      {
-        bool wanthyphen = false;
-        // A single word is longer than the wrap width. Split the word and
-        // append a hyphen at the end of the line. This won't normally happen,
-        // but it can eventually happen e.g. with paths or too low screen widths.
-        //! \todo make word-splitting more intelligent, e.g. if the word already
-        //!       contains a hyphen, split there; do not split because
-        //!       of a non-alphabet character; do not leave orphans, etc...
-        if (linep == prevwp)
-        {
-          prevwp = s - 2;
-          wanthyphen = true;
-        }
-
-        // update the size of the string to read
-        s_bytes -= (prevwp - linep);
-        // print the line, leave linep point to the start of the last word.
-        for (; linep < prevwp; ++linep)
-          out << *linep;
-        if (wanthyphen)
-          out << "-";
-        out << endl;
-        // reset column counter
-        col = 0;
-        toindent = indent;
-        // reset original text pointer (points to current wc, not the start of the word)
-        s = linep;
-        // reset shift state
-        ::memset (&shift_state, 0, sizeof (shift_state));
-      }
-      else
-        s += bytes_read;
-    }
-    // we're at the end of the string
-    else
-    {
-      // print the rest of the text
-      for(; *linep; ++linep)
-        out << *linep;
-    }
-  }
-  while(bytes_read > 0);
-}
