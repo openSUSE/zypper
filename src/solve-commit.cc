a74cdae38139d4d5dfb2b9da5b9c3080318cfe74
Michael Andres <ma@suse.de>
2016-01-22 08:46:54 +0100

    download: fixed claiming an error after successful run (bsc#956480)

--- src/solve-commit.cc
+++ src/solve-commit.cc
@@ -804,8 +804,7 @@ void solve_and_commit (Zypper & zypper)
           gData.show_media_progress_hack = false;
 	  gData.entered_commit = false;
 
-	  USR << dump(result.transaction()) << endl;
-	  if ( !result.allDone() && !( copts.count("dry-run") && result.noError() ) )
+	  if ( !result.allDone() && !( ( copts.count("dry-run") || zypper.cOpts().count("download-only") ) && result.noError() ) )
 	  { zypper.setExitCode( result.attemptToModify() ? ZYPPER_EXIT_ERR_COMMIT : ZYPPER_EXIT_ERR_ZYPP ); }	// error message comes later....
 
           MIL << endl << "DONE" << endl;
