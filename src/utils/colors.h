df336b8637dab767d0f1a94dcd4f3459ca4de5b2
Michael Andres <ma@suse.de>
2016-05-26 17:26:57 +0200

    new CCString: templated ColorString types

--- src/utils/colors.h
+++ src/utils/colors.h
@@ -51,4 +51,26 @@ namespace ansi
   { enum { customColorCtor = true }; };
 }
 
+// ColorString types
+template <ColorContext _ctxt>
+struct CCString : public ColorString
+{
+  CCString()						: ColorString( _ctxt ) {}
+  explicit CCString( const std::string & str_r )	: ColorString( _ctxt, str_r ) {}
+  explicit CCString( std::string && str_r )		: ColorString( _ctxt, std::move(str_r) ) {}
+};
+
+typedef CCString<ColorContext::DEFAULT>		DEFAULTString;
+
+typedef CCString<ColorContext::MSG_STATUS>	MSG_STATUSString;
+typedef CCString<ColorContext::MSG_ERROR>	MSG_ERRORString;
+typedef CCString<ColorContext::MSG_WARNING>	MSG_WARNINGString;
+
+typedef CCString<ColorContext::POSITIVE>	POSITIVEString;
+typedef CCString<ColorContext::CHANGE>		CHANGEString;
+typedef CCString<ColorContext::NEGATIVE>	NEGATIVEString;
+
+typedef CCString<ColorContext::HIGHLIGHT>	HIGHLIGHTString;
+typedef CCString<ColorContext::LOWLIGHT>	LOWLIGHTString;
+
 #endif /* UTILS_COLORS_H_ */
