4f6b1a10dc74731093d51c912e40731bc1e65f0b
Michael Andres <ma@suse.de>
2018-05-23 11:02:15 +0200

    fixup! zypp-NameReqPrv: enhance dependency queries

--- tools/zypp-NameReqPrv.cc
+++ tools/zypp-NameReqPrv.cc
@@ -319,20 +319,30 @@ int main( int argc, char * argv[] )
 	q.addDependency( sat::SolvAttr::provides, Capability(qstr) );
       }
       if ( requires )
+      {
 	q.addDependency( sat::SolvAttr::requires );
 	q.addDependency( sat::SolvAttr::requires, Capability(qstr) );
+      }
       if ( conflicts )
+      {
 	q.addDependency( sat::SolvAttr::conflicts );
 	q.addDependency( sat::SolvAttr::conflicts, Capability(qstr) );
+      }
       if ( obsoletes )
+      {
 	q.addDependency( sat::SolvAttr::obsoletes );
 	q.addDependency( sat::SolvAttr::obsoletes, Capability(qstr) );
+      }
       if ( recommends )
+      {
 	q.addDependency( sat::SolvAttr::recommends );
 	q.addDependency( sat::SolvAttr::recommends, Capability(qstr) );
+      }
       if ( supplements )
+      {
 	q.addDependency( sat::SolvAttr::supplements );
 	q.addDependency( sat::SolvAttr::supplements, Capability(qstr) );
+      }
       if ( enhacements )
       {
 	q.addDependency( sat::SolvAttr::enhances );
