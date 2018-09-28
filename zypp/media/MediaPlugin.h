c2c1c4491a54d1e5a0a494fe507a866e45884597
Michael Andres <ma@suse.de>
2018-09-28 11:16:21 +0200

    fix gcc warnings

--- zypp/media/MediaPlugin.h
+++ zypp/media/MediaPlugin.h
@@ -36,7 +36,7 @@ namespace zypp
       protected:
 	virtual void attachTo( bool next_r = false );
 	virtual void releaseFrom( const std::string & ejectDev_r );
-	virtual void getFile( const Pathname & filename_r, const ByteCount expectedFileSize_r ) const;
+	virtual void getFile( const Pathname & filename_r, const ByteCount &expectedFileSize_r ) const;
 	virtual void getDir( const Pathname & dirname_r, bool recurse_r ) const;
 	virtual void getDirInfo( std::list<std::string> & retlist_r, const Pathname & dirname_r, bool dots_r = true ) const;
 	virtual void getDirInfo( filesystem::DirContent & retlist_r, const Pathname & dirname_r, bool dots_r = true ) const;
