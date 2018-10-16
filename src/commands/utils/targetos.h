94cf70480dc6cbee4cad943f4fa44bd8abf0b825
Benjamin Zeller <bzeller@suse.de>
2018-10-16 16:41:09 +0200

    Migrate zypper tos to new command format

---
+++ src/commands/utils/targetos.h
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------*\
+                          ____  _ _ __ _ __  ___ _ _
+                         |_ / || | '_ \ '_ \/ -_) '_|
+                         /__|\_, | .__/ .__/\___|_|
+                             |__/|_|  |_|
+\*---------------------------------------------------------------------------*/
+#ifndef ZYPPER_COMMANDS_UTILS_TARGETOS_INCLUDED
+#define ZYPPER_COMMANDS_UTILS_TARGETOS_INCLUDED
+
+#include "commands/basecommand.h"
+#include "utils/flags/zyppflags.h"
+
+class TargetOSCmd : public ZypperBaseCommand
+{
+public:
+  TargetOSCmd( const std::vector<std::string> &commandAliases_r );
+  // ZypperBaseCommand interface
+protected:
+  zypp::ZyppFlags::CommandGroup cmdOptions() const override;
+  void doReset() override;
+  int execute(Zypper &zypp_r, const std::vector<std::string> &) override;
+
+private:
+  bool _showOSLabel = false;
+};
+
+#endif
