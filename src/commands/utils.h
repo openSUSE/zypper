a5b906e3bbb754d59123e2925e8007b3b128711b
Benjamin Zeller <bzeller@suse.de>
2018-10-16 17:20:40 +0200

    Migrate zypper versioncmp to new command format

--- src/commands/utils.h
+++ src/commands/utils.h
@@ -2,5 +2,6 @@
 #define ZYPPER_COMMANDS_UTILS_H_INCLUDED
 
 #include "utils/targetos.h"
+#include "utils/versioncmp.h"
 
 #endif
---
+++ src/commands/utils/versioncmp.h
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------------------------*\
+                          ____  _ _ __ _ __  ___ _ _
+                         |_ / || | '_ \ '_ \/ -_) '_|
+                         /__|\_, | .__/ .__/\___|_|
+                             |__/|_|  |_|
+\*---------------------------------------------------------------------------*/
+#ifndef ZYPPER_COMMANDS_UTILS_VERSIONCMP_INCLUDED
+#define ZYPPER_COMMANDS_UTILS_VERSIONCMP_INCLUDED
+
+#include "commands/basecommand.h"
+#include "utils/flags/zyppflags.h"
+
+class VersionCompareCmd : public ZypperBaseCommand
+{
+public:
+  VersionCompareCmd( const std::vector<std::string> &commandAliases_r );
+
+  // ZypperBaseCommand interface
+protected:
+  zypp::ZyppFlags::CommandGroup cmdOptions() const override;
+  void doReset() override;
+  int execute(Zypper &zypp_r, const std::vector<std::string> &positionalArgs_r) override;
+
+private:
+  bool _missingReleaseNrAsAnyRelease = false;
+};
+
+#endif
