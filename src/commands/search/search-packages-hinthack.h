cc5f3c60ed6af05e725bb32ad75192ad8fc0bce8
Michael Andres <ma@suse.de>
2018-11-26 13:24:56 +0100

    Optionally run "zypper search-packages" after "search" (FATE#325599)

--- src/commands/search/search-packages-hinthack.h
+++ src/commands/search/search-packages-hinthack.h
@@ -9,7 +9,17 @@
 
 class Zypper;
 
-/** Hack for bsc#1089994: Hinting to the zypper-search-packages-plugin subcommand. */
-void SLE15_SearchPackagesHintHack( Zypper & zypper );
+///////////////////////////////////////////////////////////////////
+namespace searchPackagesHintHack
+{
+  extern int argvCmdIdx;	///< opt parser remembered argv index of the command name
+  extern int argvArgIdx;	///< opt parser remembered argv index of 1st positional arg (or argc if none)
 
+  /** Hack for bsc#1089994: SLE15: Hinting to the \c search-packages subcommand (package \c zypper-search-packages-plugin).
+   * FATE#325599: Introduced with SLE15-SP1 and forces us to even call the plugin at the end of a search command.
+   */
+  void callOrNotify( Zypper & zypper_r );
+
+} // searchPackagesHintHack
+///////////////////////////////////////////////////////////////////
 #endif // ZYPPER_COMMANDS_SEARCH_SEARCH_PACKAGES_HINTHACK_H_INCLUDED
