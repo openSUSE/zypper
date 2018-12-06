953b3ce827e4fb72dbe42c34d9557144cabc286b
Michael Andres <ma@suse.de>
2018-12-06 17:31:52 +0100

    BuildRequires:  libsolv-devel >= 0.7.2 (SOLVER_FLAG_ONLY_NAMESPACE_RECOMMENDED)

--- libzypp.spec.cmake
+++ libzypp.spec.cmake
@@ -75,7 +75,7 @@ BuildRequires:  pkgconfig
 BuildRequires:  pkg-config
 %endif
 
-BuildRequires:  libsolv-devel >= 0.7.1
+BuildRequires:  libsolv-devel >= 0.7.2
 %if 0%{?suse_version} >= 1100
 BuildRequires:  libsolv-tools
 %requires_eq    libsolv-tools
