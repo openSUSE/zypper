934a10eb425515c87a2627619f5f1e95f6573745
Michael Andres <ma@suse.de>
2019-02-18 13:55:42 +0100

    Rework Augeas class and lense.

--- src/utils/Augeas.h
+++ src/utils/Augeas.h
@@ -8,45 +8,37 @@
 #ifndef ZYPPER_UTIL_AUGEAS_H_
 #define ZYPPER_UTIL_AUGEAS_H_
 
-extern "C"
-{
-  #include <augeas.h>
-}
+#include <iosfwd>
 #include <string>
 
-#include <zypp/base/NonCopyable.h>
+#include <zypp/base/PtrTypes.h>
+#include <zypp/Pathname.h>
 
-/**
- * Zypper's wrapper around Augeas.
- */
-class Augeas : private zypp::base::NonCopyable
+struct augeas;
+
+///////////////////////////////////////////////////////////////////
+/// \class Augeas
+/// \brief Zypper's wrapper around Augeas.
+///////////////////////////////////////////////////////////////////
+class Augeas
 {
+  NON_COPYABLE_BUT_MOVE( Augeas );
+  friend std::ostream & operator<<( std::ostream & str_r, const Augeas & obj_r );
+
 public:
-  Augeas( const std::string & file = "" );
+  /** Ctor opt. taking a custom config file (otherwise the default cfg files are used). */
+  Augeas( zypp::Pathname customcfg_r = zypp::Pathname() );
+
   ~Augeas();
 
-  std::string get( const std::string & augpath ) const;
+public:
+  /** Returns the value for \a option_r ("SECTION/VARIABLE") or an empty string. */
+  std::string getOption( const std::string & option_r ) const;
 
-  std::string getOption( const std::string & option ) const;
-
-  ::augeas * getAugPtr()
-  { return _augeas; }
-
+public:
+  class Impl;
 private:
-  std::string userOptionPath( const std::string & section, const std::string & option ) const;
-
-private:
-  ::augeas * _augeas;
-  std::string _homedir;
-  /**
-   * Path of the config file in the augeas tree,
-   * e.g. /files/path/to/user/zypper.conf
-   */
-  std::string _user_conf_path;
-  bool _got_global_zypper_conf;
-  bool _got_user_zypper_conf;
-
-  mutable int _last_get_result;
+  zypp::RW_pointer<Impl,zypp::rw_pointer::Scoped<Impl>> _pimpl; ///< Pointer to implementation
 };
 
 #endif /* ZYPPER_UTIL_AUGEAS_H_ */
