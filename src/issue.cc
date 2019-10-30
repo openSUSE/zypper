fd4f3ed2c9886ef666c2f867edebc154e86d1b3d
Michael Andres <ma@suse.de>
2019-10-30 16:03:02 +0100

    Issue: add Stream output operator

--- src/issue.cc
+++ src/issue.cc
@@ -4,8 +4,12 @@
                          /__|\_, | .__/ .__/\___|_|
                              |__/|_|  |_|
 \*---------------------------------------------------------------------------*/
+#include <iostream>
 #include "issue.h"
 
 Issue::Issue(std::string issueType_r, std::string issueId_r)
   : std::pair<std::string, std::string>( std::move(issueType_r), std::move(issueId_r) )
 {}
+
+std::ostream & operator<<( std::ostream & str, const Issue & obj )
+{ return str << "Issue(" << (obj.anyType()?"ANY":obj.type()) << "#" << (obj.anyId()?"ANY":obj.id()) << ")"; }
