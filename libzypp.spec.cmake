c3c3bb3d38bdf0972dcc35ace52665b375f31fd9
Benjamin Zeller <bzeller@suse.de>
2020-09-14 16:50:36 +0200

    BuildRequires:  libsolv-devel >= 0.7.15
    Require new APIs for writing the new style testcases

--- libzypp.spec.cmake
+++ libzypp.spec.cmake
@@ -78,7 +78,7 @@ BuildRequires:  pkgconfig
 BuildRequires:  pkg-config
 %endif
 
-BuildRequires:  libsolv-devel >= 0.7.11
+BuildRequires:  libsolv-devel >= 0.7.15
 %if 0%{?suse_version} >= 1100
 BuildRequires:  libsolv-tools
 %requires_eq    libsolv-tools
