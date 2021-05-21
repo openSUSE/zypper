beb0e764a86e7effc13cde04ff3db652c5c758a4
Michael Andres <ma@suse.de>
2021-05-21 10:35:48 +0200

    Downloader: Parse keyhints from master index into VerifyFileContext

--- zypp/repo/Downloader.cc
+++ zypp/repo/Downloader.cc
@@ -14,7 +14,11 @@
 
 #include "Downloader.h"
 #include <zypp/KeyContext.h>
+#include <zypp/ZConfig.h>
+#include <zypp/ZYppFactory.h>
 #include <zypp/ZYppCallbacks.h>
+
+#include <zypp/parser/yum/RepomdFileReader.h>
 
 using std::endl;
 
@@ -22,6 +26,94 @@ namespace zypp
 {
 namespace repo
 {
+  class ExtraSignatureFileChecker : public SignatureFileChecker
+  {
+  public:
+    /** Called after download but before verifying the file. */
+    typedef function<void( const Pathname & file_r )> PreCheckCB;
+
+    ExtraSignatureFileChecker()
+    {}
+
+    ExtraSignatureFileChecker( Pathname signature_r )
+    : SignatureFileChecker( std::move(signature_r) )
+    {}
+
+    void preCheckCB( PreCheckCB cb_r )
+    { _preCheckCB = std::move(cb_r); }
+
+    void operator()( const Pathname & file_r ) const
+    {
+      if ( _preCheckCB )
+	_preCheckCB( file_r );
+      SignatureFileChecker::operator()( file_r );
+    }
+
+  private:
+    PreCheckCB _preCheckCB;
+  };
+
+  // bsc#1184326: Check and handle extra gpg keys delivered with trusted signed master index.
+  void checkExtraKeysInRepomd( MediaSetAccess & media_r, const Pathname & destdir_r, const Pathname & repomd_r, SignatureFileChecker & sigchecker_r )
+  {
+    std::vector<std::pair<std::string,std::string>> keyhints { zypp::parser::yum::RepomdFileReader(repomd_r).keyhints() };
+    if ( keyhints.empty() )
+      return;
+    DBG << "Check keyhints: " << keyhints.size() << endl;
+
+    auto keyRing { getZYpp()->keyRing() };
+    for ( const auto & p : keyhints ) try {
+      const std::string & file  { p.first };
+      const std::string & keyid { p.second };
+
+      if ( keyRing->trustedPublicKeyData( keyid ) ) {
+	DBG << "Keyhint is already trusted: " << keyid << " (" << file << ")" << endl;
+	continue;	// already a trusted key
+      }
+
+      DBG << "Keyhint search key " << keyid << " (" << file << ")" << endl;
+      PublicKeyData keyData = keyRing->publicKeyData( keyid );
+      if ( not keyData ) {
+	// try to get it from cache or download it...
+
+	// TODO: Enhance the key caching in general...
+	const ZConfig & conf = ZConfig::instance();
+	Pathname cacheFile = conf.repoManagerRoot() / conf.pubkeyCachePath() / file;
+
+	PublicKey key { PublicKey::noThrow( cacheFile ) };
+	if ( not key.fileProvidesKey( keyid ) ) {
+
+	  key = PublicKey::noThrow( media_r.provideOptionalFile( file ) );
+	  if ( not key.fileProvidesKey( keyid ) ) {
+
+	    WAR << "Keyhint " << file << " does not contain a key with id " << keyid << ". Skipping it." << endl;
+	    continue;
+	  }
+	  // Try to cache it...
+	  filesystem::hardlinkCopy( key.path(), cacheFile );
+	}
+
+	keyRing->importKey( key, false );		// store in general keyring (not trusted!)
+	keyData = keyRing->publicKeyData( keyid );	// fetch back from keyring in case it was a hidden key
+      }
+
+      if ( not PublicKey::isSafeKeyId( keyid ) ) {
+	WAR << "Keyhint " << keyid << " for " << keyData << " is not strong enough for auto import. Just caching it." << endl;
+	continue;
+      }
+
+      DBG << "Keyhint remember buddy " << keyData << endl;
+      sigchecker_r.addBuddyKey( keyid );
+    }
+    catch ( const Exception & exp )
+    { ZYPP_CAUGHT(exp); }
+    catch ( const std::exception & exp )
+    { ZYPP_CAUGHT(exp); }
+    catch (...)
+    { INT << "Oops!" << endl; }
+    MIL << "Check keyhints done. Buddy keys: " << sigchecker_r.buddyKeys().size() << endl;
+  }
+
 
 Downloader::Downloader()
 {
@@ -58,7 +150,7 @@ void Downloader::defaultDownloadMasterIndex( MediaSetAccess & media_r, const Pat
   reset();
 
   FileChecker checker;	// set to sigchecker if appropriate, else Null.
-  SignatureFileChecker sigchecker;
+  ExtraSignatureFileChecker sigchecker;
   bool isSigned = PathInfo(destdir_r / sigpath).isExist();
 
   if ( repoInfo().repoGpgCheck() )
@@ -77,6 +169,21 @@ void Downloader::defaultDownloadMasterIndex( MediaSetAccess & media_r, const Pat
       // (unsigned repo, key file missing; bnc #495977)
       sigchecker.keyContext( repoInfo() );
 
+      // bsc#1184326: Check and handle extra gpg keys delivered with trusted signed master index.
+      if ( masterIndex_r.basename() == "repomd.xml" ) {
+	sigchecker.preCheckCB( [&]( const Pathname & file_r )->void {
+	  // Take care no exception escapes! Main job is the signature verification.
+	  try {
+	    checkExtraKeysInRepomd( media_r, destdir_r, file_r, sigchecker );
+	  }
+	  catch ( const Exception & exp )
+	  { ZYPP_CAUGHT(exp); }
+	  catch ( const std::exception & exp )
+	  { ZYPP_CAUGHT(exp); }
+	  catch (...)
+	  { INT << "Oops!" << endl; }
+	});
+      }
       checker = FileChecker( ref(sigchecker) );	// ref() to the local sigchecker is important as we want back fileValidated!
     }
     else
