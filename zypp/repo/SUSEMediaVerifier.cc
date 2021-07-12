openSUSE/libzypp

Make OnMediaLocation the default argument to provide files
This patch enables OnMediaLocation on all levels in the media backend.
All other provide functions are deprecated. OnMediaLoction will make it
straight forward to transfer additional information from the user code
into the media handlers and by that introduce new features without
having to change each level by hand.

zypp/repo/SUSEMediaVerifier.cc
@@ -52,7 +52,7 @@ bool SUSEMediaVerifier::isDesiredMedia(const media::MediaHandler &ref)
     return true;
 
   Pathname media_file = "/media." + str::numstring(_media_nr) + "/media";
-  ref.provideFile (media_file, 0);
+  ref.provideFile ( OnMediaLocation(media_file) );
   media_file = ref.localPath(media_file);
   std::ifstream str(media_file.asString().c_str());
   std::string vendor;

openSUSE/zypper

zypp/repo/SUSEMediaVerifier.cc
@@ -165,7 +165,7 @@ namespace zypp
 
       Pathname mediaFile { _pimpl->mediaFilePath() };
       try {
-	ref.provideFile( mediaFile, 0 );
+	ref.provideFile( OnMediaLocation(mediaFile) );
 	mediaFile = ref.localPath( mediaFile );
       }
       catch ( media::MediaFileNotFoundException & excpt_r )