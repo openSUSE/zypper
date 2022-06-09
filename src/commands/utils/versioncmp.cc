b90d4a71d31f477efd0c5c84303662095aef8806
Michael Andres <ma@suse.de>
2022-06-09 15:10:44 +0200

    versioncmp: if verbose, also print the edition 'parts' whci are compared

--- src/commands/utils/versioncmp.cc
+++ src/commands/utils/versioncmp.cc
@@ -78,5 +78,70 @@ int VersionCompareCmd::execute( Zypper &zypper, const std::vector<std::string> &
   else
     zypper.out().info( str::form(_("%s is older than %s"), lhs.asString().c_str(), rhs.asString().c_str() ) );
 
+  if ( zypper.out().verbosity() > Out::NORMAL ) {
+    auto chclass = [] ( char ch )->unsigned {
+      if ( ch >= '0' && ch <= '9' )
+        return 1;
+      if ( (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') )
+        return 2;
+      if ( ch == '~' )
+        return 3;
+      if ( ch == '^' )
+        return 4;
+      return 0;
+    };
+    auto split = [&] ( const std::string & v, std::vector<std::string_view> & vs, std::string_view first ) {
+      vs.clear();
+      vs.push_back( first );
+      for ( const char * s = v.c_str(); *s; ) {
+        unsigned cs = chclass( *s );
+        if ( cs == 0 ) {
+          ++s;
+          continue;
+        }
+        const char * e = s+1;
+        while ( chclass( *e ) == cs )
+          ++e;
+        vs.push_back( std::string_view(s,e-s) );
+        s = e;
+      }
+    };
+    auto printpadded = [] ( std::ostream & str, const std::vector<std::string_view> & vs, const std::vector<unsigned> & width ) ->std::ostream & {
+      for ( unsigned i = 0; i < vs.size(); ++i ) {
+        if ( i < width.size() ) {
+          for ( unsigned pad = width[i] - vs[i].size(); pad; --pad )
+            str << " ";
+        }
+        str << vs[i] << " : ";
+      }
+      return str;
+    };
+    auto print = [&] ( std::ostream & str, const std::vector<std::string_view> & ls, const std::vector<std::string_view> & rs ) {
+      std::vector<unsigned> width;
+      for ( unsigned i = 0; i < ls.size() && i < rs.size(); ++i ) {
+        width.push_back( std::max( ls[i].size(), rs[i].size() ) );
+      }
+      printpadded( str, ls, width ) << endl;
+      printpadded( str, rs, width ) << endl;
+    };
+    std::vector<std::string_view> ls;
+    std::vector<std::string_view> rs;
+    std::string l { lhs.version() };
+    std::string r { rhs.version() };
+    auto str { zypper.out().info() };
+
+    split( l, ls, "LHSversion:" );
+    split( r, rs, "RHSversion:" );
+    print( str << "", ls, rs );
+
+    l = lhs.release();
+    r = rhs.release();
+    if ( l.size() || r.size() ) {
+      split( l, ls, "LHSrelease:" );
+      split( r, rs, "RHSrelease:" );
+      print( str << "", ls, rs );
+    }
+  }
+
   return ZYPPER_EXIT_OK;
 }
