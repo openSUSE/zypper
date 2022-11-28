401a8cc1255b7e945b55946fe21527a49b40fc87
Benjamin Zeller <bzeller@suse.de>
2022-11-28 15:09:31 +0100

    Disable explicit pipelining in CURL
    Setting curl_multi_setopt( _multi, CURLMOPT_PIPELINING,
    CURLPIPE_MULTIPLEX|CURLPIPE_HTTP1 ) seems to have issues
    with older libcurl versions. Very long delays can happen
    causing out activity timout to trigger a request failure.
    Disabling it for now.

--- zypp-curl/ng/network/networkrequestdispatcher.cc
+++ zypp-curl/ng/network/networkrequestdispatcher.cc
@@ -56,7 +56,10 @@ NetworkRequestDispatcherPrivate::NetworkRequestDispatcherPrivate(  NetworkReques
   curl_multi_setopt( _multi, CURLMOPT_TIMERDATA, reinterpret_cast<void *>( this ) );
   curl_multi_setopt( _multi, CURLMOPT_SOCKETFUNCTION, NetworkRequestDispatcherPrivate::static_socket_callback );
   curl_multi_setopt( _multi, CURLMOPT_SOCKETDATA, reinterpret_cast<void *>( this ) );
-  curl_multi_setopt( _multi, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX|CURLPIPE_HTTP1 );
+
+  // disabled explicit pipelining since it breaks our tests on releases < 15.2
+  // we could consider enabling it starting with a specific CURL version
+  // curl_multi_setopt( _multi, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX|CURLPIPE_HTTP1 );
 
   _timer->setSingleShot( true );
   _timer->connect( &Timer::sigExpired, *this, &NetworkRequestDispatcherPrivate::multiTimerTimout );
